32fdbbc2f430804162c7212b2b8fbec537a69f64
Michael Wu <michaelwu@Michaels-MacBook-Pro.local>
2019-03-11 13:07:25 -0700

    config file read in app.py done

--- optimade/adapter/app.py
+++ optimade/adapter/app.py
@@ -6,6 +6,7 @@ from util import *
 import os, subprocess
 from models_class import *
 from models_schema import *
+import configparser
 
 app = Flask(__name__)
 app.config['TESTING'] = True
@@ -23,17 +24,34 @@ def find():
     # Test 5(no limit) --> http://127.0.0.1:5000/optimade/0.9.6/structures?filter=nelements%3C3&response_format=jsonapi&email_address=dwinston%40lbl.gov&response_fields=id%2Cnelements%2Cmaterial_id%2Celements%2Cformula_prototype&sort=-nelements
 
     # current --> http://127.0.0.1:5000/optimade/0.9.6/structures/?filter=nelements%3C3&response_format=jsonapi&email_address=dwinston%40lbl.gov&response_limit=5&response_fields=id%2Cnelements%2Cmaterial_id%2Celements%2Cformula_prototype&sort=-nelements
-    alias = {
-        "chemical_formula":"formula_anonymous",
-        "formula_prototype": "pretty_formula",
-    }
+    # TODO: CONFIG PARSER
+    # alias = {
+    #     "chemical_formula":"formula_anonymous",
+    #     "formula_prototype": "pretty_formula",
+    # }
+
+
+    #### START OF READING ALIAS FROM CONFIG  ####
+    config = configparser.ConfigParser()
+    config.read("./config.ini")
+    class ConfigFileNotFoundException(Exception):
+        pass
+    if(not (config.has_section('aliases') )):
+        raise ConfigFileNotFoundException("Config File Not Found at Location: {}".format(args.Config))
+    alias = dict()
+    if(config.has_section('aliases')):
+        d = dict(config.items('aliases'))
+        for key in d:
+            alias[key] = config['aliases'][key]
+    #### END OF READING ALIAS FROM CONFIG  ####
+
+    #### START OF DATABASE CONNECTION  ####
     client = MongoClient()
     db=client.test_database
     test_collection = db.test_collection
-    query = dict(request.args)
-    result = getResponse(test_collection, request.url, alias)
+    #### END OF DATABASE CONNECTION  ####
 
-    pprint(result)
+    result = getResponse(test_collection, request.url, alias)
 
     return str(result)
 
