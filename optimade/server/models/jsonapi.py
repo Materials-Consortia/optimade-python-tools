e182a371b50e35acb46ab25a170552433028cf25
Thomas Purcell <purcell@fhi-berlin.mpg.de>
2019-06-12 16:20:31 +0200

    Adding JSON API
    Adding the JSON API to the spec generator, need to figure out how to add patternProperties

--- optimade/server/models/jsonapi.py
+++ optimade/server/models/jsonapi.py
@@ -1,7 +1,7 @@
 """
 This module should reproduce https://jsonapi.org/schema
 """
-from typing import Optional
+from typing import Optional, List, Union
 
 from pydantic import BaseModel, UrlStr
 
@@ -10,11 +10,70 @@ class Link(BaseModel):
     href: UrlStr
     meta: Optional[dict]
 
-
 class Links(BaseModel):
     next: Optional[Link]
-
 
 class Resource(BaseModel):
     id: str
     type: str
+    attributes: Optional[Attributes]
+    relationships : Optional[Relationships]
+    links: Optional[Links]
+    meta: Optional[dict]
+
+class Success(BaseModel):
+    data: Data
+    included: Optional[List[Resource]]
+    uniqueItems: bool = True
+    meta: Optional[dict]
+    links: Optional[Union[Links, Pagination]]
+    jsonapi: Optional[Jsonapi]
+
+class Failure(BaseModel):
+    errors: List[Error]
+    meta: Optional[dict]
+    jsonapi: Optional[Jsonapi]
+    links: Optional[Links]
+
+class Info(BaseModel):
+    meta: dict
+    jsonapi: Optional[Jsonapi]
+    links: Optional[Links]
+
+class Data(List[Resource]):
+
+class RelationshipLinks(BaseModel):
+    self: Optional[Link]
+    related: Optional[Link]
+
+class Attributes(BaseModel):
+
+class Relationships(BaseModel):
+
+class RelationshipToOne(BaseModel):
+
+class RelationshipToMany(BaseModel):
+
+class Empty(None):
+
+class Linkage(BaseModel):
+    type: str
+    id: str
+    meta: Optional[dict]
+
+class Pagination(BaseModel):
+    first: Optional[UrlStr, None]
+    last: Optional[UrlStr, None]
+    prev: Optional[UrlStr, None]
+    next: Optional[UrlStr, None]
+
+class Jsonapi(BaseModel):
+    version: str
+    meta: Optional[dict]
+class Error(BaseModel):
+    id: str
+    status: str
+    code: str
+    title: str
+    detail: str
+    meta: dict
