a83e4209cead4a9bf1fafb066fa07531c1eb5f7d
Thomas Purcell <tpurcell90@users.noreply.github.com>
2019-06-13 16:43:24 +0200

    Json api add (#24)
    * Adding JSON API
    
    Adding the JSON API to the spec generator
    
    Relationships are now added to the jsonapi file, but does not work in our tests.
    If you print the pydantic schema it works as I would expect, but it is not working in
    openapi.json
    
    JSON API works outside of the patternProperties Bug
    
    JSON API should now agree, outside of the bug where print CLASS.schema() and openapi.json
    disagree

--- optimade/server/models/jsonapi.py
+++ optimade/server/models/jsonapi.py
@@ -1,21 +1,85 @@
 """
 This module should reproduce https://jsonapi.org/schema
 """
-from typing import Optional
 
-from pydantic import BaseModel, UrlStr, Union
-
+from typing import Optional, Set, Union, Dict, Any
+from pydantic import BaseModel, UrlStr, constr
 
 class Link(BaseModel):
     href: UrlStr
     meta: Optional[dict]
 
-
 class Links(BaseModel):
     next: Optional[Union[UrlStr,Link]]
     base_url: Optional[Union[UrlStr,Link]]
 
+class JsonAPI(BaseModel):
+    version: str
+    meta: Optional[dict]
+
+class Pagination(BaseModel):
+    first: Optional[UrlStr]
+    last: Optional[UrlStr]
+    prev: Optional[UrlStr]
+    next: Optional[UrlStr]
+
+class Source(BaseModel):
+    pointer: Optional[str]
+    parmeter: Optional[str]
+
+class Error(BaseModel):
+    id: Optional[str]
+    status: Optional[str]
+    code: Optional[str]
+    title: Optional[str]
+    detail: Optional[str]
+    source: Optional[Source]
+    meta: Optional[dict]
+
+class Failure(BaseModel):
+    errors: Set[Error]
+    meta: Optional[dict]
+    jsonapi: Optional[JsonAPI]
+    links: Optional[Links]
+
+class Info(BaseModel):
+    meta: dict
+    jsonapi: Optional[JsonAPI]
+    links: Optional[Links]
+
+att_pat_prop = constr(regex=r'^(?!relationships$|links$|id$|type$)\\w[-\\w_]*$')
+class Attributes(BaseModel):
+    items: Optional[Dict[att_pat_prop, Any]]
+
+class RelationshipLinks(BaseModel):
+    self: Optional[Link]
+    related: Optional[Link]
+
+class Linkage(BaseModel):
+    type: str
+    id: str
+    meta: Optional[dict]
+
+class Relationship(BaseModel):
+    links: Optional[RelationshipLinks]
+    data: Optional[Union[Linkage, Set[Linkage]]]
+    meta: Optional[dict]
+
+rel_pat_prop = constr(regex=r"^(?!id$|type$)\\w[-\\w_]*$")
+class Relationships(BaseModel):
+    items : Optional[Dict[rel_pat_prop, Relationship]]
 
 class Resource(BaseModel):
     id: str
     type: str
+    links: Optional[Links]
+    meta: Optional[dict]
+    attributes: Optional[Attributes]
+    relationships: Optional[Relationships]
+
+class Success(BaseModel):
+    data: Union[None, Resource, Set[Resource]]
+    included: Optional[Set[Resource]]
+    meta: Optional[dict]
+    links: Optional[Union[Links, Pagination]]
+    jsonapi: Optional[JsonAPI]
