328ff3c6fe6f3658c9451e1657371c160c30651f
Thomas Purcell <purcell@fhi-berlin.mpg.de>
2019-06-14 10:58:06 +0200

    Added documentation to jsonapi
    Put in docstrings into jsonapi file

--- optimade/server/models/jsonapi.py
+++ optimade/server/models/jsonapi.py
@@ -8,29 +8,51 @@ class Meta(Dict[str, Any]):
     """Non-standard meta-information that can not be represented as an attribute or relationship."""
 
 class Link(BaseModel):
+    """A link **MUST** be represented as either: a string containing the link's URL or a link object."""
     href: UrlStr
     meta: Optional[dict]
 
 class Links(BaseModel):
+    """A Links object is a set of keys with a Link value"""
     next: Optional[Union[UrlStr,Link]]
     self: Optional[Union[UrlStr,Link]]
     about: Optional[Union[UrlStr, Link]]
 
 class JsonAPI(BaseModel):
+    """An object describing the server's implementation"""
     version: str
     meta: Optional[dict]
 
 class Pagination(BaseModel):
+    """
+    A set of urls to different pages:
+        first:The first page of data
+        last:The last page of data
+        prev:The previous page of data
+        next:The next page of data
+    """
     first: Optional[UrlStr]
     last: Optional[UrlStr]
     prev: Optional[UrlStr]
     next: Optional[UrlStr]
 
 class Source(BaseModel):
+    """A source object used in the Error type"""
     pointer: Optional[str]
     parmeter: Optional[str]
 
 class Error(BaseModel):
+    """
+    An error response
+    id: A unique identifier for this particular occurrence of the problem.
+    links: A links object
+    status: The HTTP status code applicable to this problem, expressed as a string value.
+    code: An application-specific error code, expressed as a string value.
+    title: A short, human-readable summary of the problem. It **SHOULD NOT** change from occurrence to occurrence of the problem, except for purposes of localization.
+    detail: A human-readable explanation specific to this occurrence of the problem.
+    source: A source
+    meta: A meta dict
+    """
     id: Optional[str]
     status: Optional[str]
     code: Optional[str]
@@ -41,30 +63,36 @@ class Error(BaseModel):
     links: Optional[Links]
 
 class Failure(BaseModel):
+    """A failure object"""
     errors: Set[Error]
     meta: Optional[dict]
     jsonapi: Optional[JsonAPI]
     links: Optional[Links]
 
 class Info(BaseModel):
+    """Information dict about the API"""
     meta: dict
     jsonapi: Optional[JsonAPI]
     links: Optional[Links]
 
 att_pat_prop = constr(regex=r'^(?!relationships$|links$|id$|type$)\\w[-\\w_]*$')
 class Attributes(BaseModel):
+    """Members of the attributes object (\"attributes\") represent information about the resource object in which it's defined."""
     items: Optional[Dict[att_pat_prop, Any]]
 
 class RelationshipLinks(BaseModel):
+    """A resource object **MAY** contain references to other resource objects (\"relationships\"). Relationships may be to-one or to-many. Relationships can be specified by including a member in a resource's links object."""
     self: Optional[Link]
     related: Optional[Link]
 
 class Linkage(BaseModel):
+    """The \"type\" and \"id\" to non-empty members."""
     type: str
     id: str
     meta: Optional[dict]
 
 class Relationship(BaseModel):
+    """Representation of one reference from the resource object in which it's defined to other resource objects."""
     links: Optional[RelationshipLinks]
     data: Optional[Union[Linkage, Set[Linkage]]]
     meta: Optional[dict]
@@ -80,9 +108,11 @@ class RelationshipToMany(Set[Linkage]):
 
 rel_pat_prop = constr(regex=r"^(?!id$|type$)\\w[-\\w_]*$")
 class Relationships(BaseModel):
+    """Members of the relationships object (\"relationships\") represent references from the resource object in which it's defined to other resource objects."""
     items : Optional[Dict[rel_pat_prop, Relationship]]
 
 class Resource(BaseModel):
+    """\"Resource objects\" appear in a JSON:API document to represent resources."""
     id: str
     type: str
     links: Optional[Links]
@@ -91,6 +121,7 @@ class Resource(BaseModel):
     relationships: Optional[Relationships]
 
 class Success(BaseModel):
+    """A Successful response"""
     data: Union[None, Resource, Set[Resource]]
     included: Optional[Set[Resource]]
     meta: Optional[dict]
--- optimade/server/models/modified_jsonapi.py
+++ optimade/server/models/modified_jsonapi.py
@@ -7,15 +7,19 @@ from pydantic import  UrlStr, BaseModel
 from typing import Optional, Union, Any
 
 class Attributes(jsonapi.Attributes):
+    """Modification of Attributes to include Optimade specified keys"""
     local_id: UrlStr
     lad_modified: datetime
     immutable_id: Optional[UrlStr]
 
 class ErrorLinks(BaseModel):
+    """Links with recast for Errors"""
     about: Union[jsonapi.Link, UrlStr]
 
 class Error(jsonapi.Error):
+    """Error where links uses ErrorLinks"""
     links: Optional[ErrorLinks]
 
 class Links(jsonapi.Links):
+    """Links now store base_url"""
     base_rul: Optional[Union[jsonapi.Link, UrlStr]]
