3c2874e4cedae2e8743984b11a0d9b0375a008af
Markus Scheidgen <markus@dhcp-46-238.physik.hu-berlin.de>
2019-09-26 15:13:53 +0200

    Added extra for server.

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     version="0.2.0",
     packages=find_packages(),
     include_package_data=True,
+    package_data={'': ['*.g']},
     url="https://github.com/Materials-Consortia/optimade-python-tools",
     license="MIT",
     author="OPTiMaDe Development Team",
@@ -16,14 +17,11 @@ setup(
     description="Tools for implementing and consuming OPTiMaDe APIs.",
     long_description=open(os.path.join(module_dir, "README.md")).read(),
     long_description_content_type="text/markdown",
-    install_requires=[
-        "pymongo>=3.8",
-        "lark-parser>=0.5.6",
-        "mongogrant",
-        "mongomock>=3.16",
-        "fastapi[all]",
-    ],
-    extras_require={"dev": ["black", "invoke", "pre-commit", "twine"]},
+    install_requires=["lark-parser>=0.5.6"],
+    extras_require={
+        "dev": ["black", "invoke", "pre-commit", "twine"],
+        "server": ["pymongo>=3.8", "mongogrant", "mongomock>=3.16", "fastapi[all]"]
+    },
     tests_require=["pytest>=3.6", "openapi-spec-validator", "jsondiff"],
     classifiers=[
         "Development Status :: 3 - Alpha",
