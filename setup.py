eed9896d1b0560a9656a30026cf019316e68dd9e
Matthew Evans <me388@cam.ac.uk>
2019-10-23 18:37:47 +0100

    Reverted changes to setup.py

--- setup.py
+++ setup.py
@@ -5,27 +5,6 @@ from setuptools import setup, find_packages
 
 module_dir = os.path.dirname(os.path.abspath(__file__))
 
-with open("requirements.txt") as f:
-    requirements = [
-        line.strip()
-        for line in f.readlines()
-        if line and not line.strip().startswith("#")
-    ]
-
-extra_requirements = {}
-for fname in glob.glob("requirements/*_requirements.txt"):
-    req = os.path.basename(fname).split("_")[0]
-    with open(fname, "r") as f:
-        extra_requirements[req] = [
-            line.strip()
-            for line in f.readlines()
-            if line and not line.strip().startswith("#")
-        ]
-
-extra_requirements["all"] = [
-    req for key in extra_requirements for req in extra_requirements[key]
-]
-
 setup(
     name="optimade",
     version="0.2.0",
@@ -38,8 +17,18 @@ setup(
     description="Tools for implementing and consuming OPTiMaDe APIs.",
     long_description=open(os.path.join(module_dir, "README.md")).read(),
     long_description_content_type="text/markdown",
-    install_requires=requirements,
-    extras_require=extra_requirements,
+    install_requires=[
+        "pymongo>=3.8",
+        "lark-parser>=0.5.6",
+        "mongogrant",
+        "mongomock>=3.16",
+        "fastapi[all]",
+    ],
+    extras_require={
+        "dev": ["black", "invoke", "pre-commit", "twine"],
+        "mongo": ["mongogrant"],
+        "django": ["django==2.2.5"],
+    },
     tests_require=["pytest>=3.6", "openapi-spec-validator", "jsondiff"],
     classifiers=[
         "Development Status :: 3 - Alpha",
