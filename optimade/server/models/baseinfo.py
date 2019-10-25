5f003726437c397545a1d7e0f51e214328fd08f0
Matthew Evans <me388@cam.ac.uk>
2019-10-25 18:12:08 +0100

    Fixes of default values to info endpoint

--- optimade/server/models/baseinfo.py
+++ optimade/server/models/baseinfo.py
@@ -1,18 +1,19 @@
-from typing import Dict, List
+from typing import Dict, List, Optional
 
-from pydantic import BaseModel, UrlStr
+from pydantic import BaseModel, UrlStr, Schema
 from .modified_jsonapi import Resource
 
 
 class BaseInfoAttributes(BaseModel):
-    api_version: str
+    api_version: str = Schema(...)
     available_api_versions: Dict[str, UrlStr]
     formats: List[str] = ["jsonapi"]
     entry_types_by_format: Dict[str, List[str]]
-    available_endpoints: List[str] = ["structure", "all", "info"]
+    available_endpoints: List[str] = Schema(...)
+    is_index: Optional[bool] = Schema(default=False, const=True)
 
 
 class BaseInfoResource(Resource):
-    id: str = "/"
-    type: str = "info"
-    attributes: BaseInfoAttributes
+    id: str = Schema(default="/", const=True)
+    type: str = Schema(default="info", const=True)
+    attributes: BaseInfoAttributes = Schema(...)
