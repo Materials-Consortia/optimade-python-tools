c2399bb12cd154e35d32ca981313c79f7fe0ea5b
Matthew Evans <me388@cam.ac.uk>
2019-10-25 18:22:51 +0100

    Fixed deviation from schema relating to elements field

--- optimade/server/models/structures.py
+++ optimade/server/models/structures.py
@@ -114,8 +114,12 @@ the values not to sum to one are the same as those specified for the
 
 
 class StructureResourceAttributes(EntryResourceAttributes):
+    """ This class contains the Schema for the attributes used to represent
+    a structure, e.g. unit cell, atoms, positions.
 
-    elements: str = Schema(
+    """
+
+    elements: List[str] = Schema(
         ...,
         description="""Names of elements found in the structure as a list of strings,
 in alphabetical order.
