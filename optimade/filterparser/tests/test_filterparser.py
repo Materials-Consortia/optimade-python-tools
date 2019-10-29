572b468a3d783250b6e1a9caaa9e26e13e29931b
Adam Fekete <adam.fekete@kcl.ac.uk>
2019-10-29 01:55:19 +0000

    adding tests

--- optimade/filterparser/tests/test_filterparser.py
+++ optimade/filterparser/tests/test_filterparser.py
@@ -40,67 +40,80 @@ class ParserTest(unittest.TestCase):
         self.assertIsNotNone(repr(self.parser))
 
 
-class FilterParserTest(unittest.TestCase):
+class ParserTestNew(unittest.TestCase):
     version = (0, 10, 0)
     variant = 'default'
-    passes = (
-        ' ',
-        'NOT ( chemical_formula_hill = "Al" AND chemical_formula_anonymous = "A" OR chemical_formula_anonymous = "H2O" AND NOT chemical_formula_hill = "Ti" )',
-        'nelements > 3',
-        'chemical_formula_hill = "H2O" AND chemical_formula_anonymous != "AB"',
-        '_exmpl_aax <= +.1e8 OR nelements >= 10 AND NOT ( _exmpl_x != "Some string" OR NOT _exmpl_a = 7)',
-        '_exmpl_spacegroup="P2"',
-        '_exmpl_cell_volume<100.0',
-        '_exmpl_bandgap > 5.0 AND _exmpl_molecular_weight < 350',
-        '_exmpl_melting_point<300 AND nelements=4 AND elements="Si,O2"',
-        '_exmpl_some_string_property = 42',
-        '5 < _exmpl_a',
-        '((NOT (_exmpl_a>_exmpl_b)) AND _exmpl_x>0)',
-        '5 < 7',
-        'chemical_formula_anonymous CONTAINS "C2" AND chemical_formula_anonymous STARTS WITH "A2"',
-        'chemical_formula_anonymous STARTS "B2" AND chemical_formula_anonymous ENDS WITH "D2"',
-        'list HAS < 3',
-        'list HAS ALL < 3, > 3',
-        'list:list HAS >=2:<=5',
-        'elements HAS "H" AND elements HAS ALL "H","He","Ga","Ta" AND elements HAS ONLY "H","He","Ga","Ta" AND elements HAS ANY "H", "He", "Ga", "Ta"',
-        'elements HAS ONLY "H","He","Ga","Ta"',
-        'elements:_exmpl_element_counts HAS "H":6 AND elements:_exmpl_element_counts HAS ALL "H":6,"He":7 AND elements:_exmpl_element_counts HAS ONLY "H":6 AND elements:_exmpl_element_counts HAS ANY "H":6,"He":7 AND elements:_exmpl_element_counts HAS ONLY "H":6,"He":7',
-        '_exmpl_element_counts HAS < 3 AND _exmpl_element_counts HAS ANY > 3, = 6, 4, != 8',
-        'elements:_exmpl_element_counts:_exmpl_element_weights HAS ANY > 3:"He":>55.3 , = 6:>"Ti":<37.6 , 8:<"Ga":0',
-        'identifier1.identifierd2 = 42',
-        'chemical_formula_hill IS KNOWN AND NOT chemical_formula_anonymous IS UNKNOWN',
-        'NOT a > b OR c = 100 AND f = "C2 H6"',
-        '(NOT (a > b)) OR ( (c = 100) AND (f = "C2 H6") )',
-        'a >= 0 AND NOT b < c OR c = 0',
-        '((a >= 0) AND (NOT (b < c))) OR (c = 0)',
-        'te < st',
-        'spacegroup="P2"',
-        '_cod_cell_volume<100.0',
-        '_mp_bandgap > 5.0 AND _cod_molecular_weight < 350',
-        '_cod_melting_point<300 AND nelements=4 AND elements="Si,O2"',
-        'number=0.ANDnumber=.0ANDnumber=0.0ANDnumber=+0ANDNUMBER=-0ANDnumber=0e1ANDnumber=0e-1ANDnumber=0e+1',
-        'key=value',
-        'author=" someone "',
-        'NOTICE=val',
-        'author="Sąžininga Žąsis"',
-        'a = 12345 AND b = +12 AND c = -34 AND d = 1.2 AND e = .2E7 AND f = -.2E+7 AND g = +10.01E-10 AND h = 6.03e23 AND i = .1E1 AND j = -.1e1 AND k = 1.e-12 AND l = -.1e-12 AND m = 1000000000.E1000000000',
-        'field = "!#$%&\'() * +, -./:; <= > ? @[] ^ `{|}~ % "',
-    )
-    fails = (
-        'number=0.0.1',
-    )
 
     def setUp(self):
         self.parser = LarkParser(version=self.version, variant=self.variant)
+        self.parse = lambda inp: self.parser.parse(inp)
 
     def test_passes(self):
-        for tf in self.passes:
-            tree = self.parser.parse(tf)
-            self.assertTrue(tree, Tree)
+        self.assertIsInstance(self.parse(' '), Tree)
+        self.assertIsInstance(
+            self.parse('NOT ( chemical_formula_hill = "Al" AND chemical_formula_anonymous = "A" OR '
+                       'chemical_formula_anonymous = "H2O" AND NOT chemical_formula_hill = "Ti" )'), Tree)
+        self.assertIsInstance(self.parse('nelements > 3'), Tree)
+        self.assertIsInstance(self.parse('chemical_formula_hill = "H2O" AND chemical_formula_anonymous != "AB"'), Tree)
+        self.assertIsInstance(
+            self.parse('_exmpl_aax <= +.1e8 OR nelements >= 10 AND '
+                       'NOT ( _exmpl_x != "Some string" OR NOT _exmpl_a = 7)'), Tree)
+        self.assertIsInstance(self.parse('_exmpl_spacegroup="P2"'), Tree)
+        self.assertIsInstance(self.parse('_exmpl_cell_volume<100.0'), Tree)
+        self.assertIsInstance(self.parse('_exmpl_bandgap > 5.0 AND _exmpl_molecular_weight < 350'), Tree)
+        self.assertIsInstance(self.parse('_exmpl_melting_point<300 AND nelements=4 AND elements="Si,O2"'), Tree)
+        self.assertIsInstance(self.parse('_exmpl_some_string_property = 42'), Tree)
+        self.assertIsInstance(self.parse('5 < _exmpl_a'), Tree)
+        self.assertIsInstance(self.parse('((NOT (_exmpl_a>_exmpl_b)) AND _exmpl_x>0)'), Tree)
+        self.assertIsInstance(self.parse('5 < 7'), Tree)
+        self.assertIsInstance(self.parse('chemical_formula_anonymous CONTAINS "C2" AND'
+                                         ' chemical_formula_anonymous STARTS WITH "A2"'), Tree)
+        self.assertIsInstance(self.parse('chemical_formula_anonymous STARTS "B2" AND '
+                                         'chemical_formula_anonymous ENDS WITH "D2"'), Tree)
+        self.assertIsInstance(self.parse('list HAS < 3'), Tree)
+        self.assertIsInstance(self.parse('list HAS ALL < 3, > 3'), Tree)
+        self.assertIsInstance(self.parse('list:list HAS >=2:<=5'), Tree)
+        self.assertIsInstance(
+            self.parse('elements HAS "H" AND elements HAS ALL "H","He","Ga","Ta" AND '
+                       'elements HAS ONLY "H","He","Ga","Ta" AND elements HAS ANY "H", "He", "Ga", "Ta"'), Tree)
+        self.assertIsInstance(self.parse('elements HAS ONLY "H","He","Ga","Ta"'), Tree)
+        self.assertIsInstance(
+            self.parse('elements:_exmpl_element_counts HAS "H":6 AND '
+                       'elements:_exmpl_element_counts HAS ALL "H":6,"He":7 AND '
+                       'elements:_exmpl_element_counts HAS ONLY "H":6 AND '
+                       'elements:_exmpl_element_counts HAS ANY "H":6,"He":7 AND '
+                       'elements:_exmpl_element_counts HAS ONLY "H":6,"He":7'), Tree)
+        self.assertIsInstance(self.parse('_exmpl_element_counts HAS < 3 AND '
+                                         '_exmpl_element_counts HAS ANY > 3, = 6, 4, != 8'), Tree)
+        self.assertIsInstance(self.parse('elements:_exmpl_element_counts:_exmpl_element_weights '
+                                         'HAS ANY > 3:"He":>55.3 , = 6:>"Ti":<37.6 , 8:<"Ga":0'), Tree)
+        self.assertIsInstance(self.parse('identifier1.identifierd2 = 42'), Tree)
+        self.assertIsInstance(self.parse('chemical_formula_hill IS KNOWN AND '
+                                         'NOT chemical_formula_anonymous IS UNKNOWN'), Tree)
+        self.assertIsInstance(self.parse('NOT a > b OR c = 100 AND f = "C2 H6"'), Tree)
+        self.assertIsInstance(self.parse('(NOT (a > b)) OR ( (c = 100) AND (f = "C2 H6") )'), Tree)
+        self.assertIsInstance(self.parse('a >= 0 AND NOT b < c OR c = 0'), Tree)
+        self.assertIsInstance(self.parse('((a >= 0) AND (NOT (b < c))) OR (c = 0)'), Tree)
+        self.assertIsInstance(self.parse('te < st'), Tree)
+        self.assertIsInstance(self.parse('spacegroup="P2"'), Tree)
+        self.assertIsInstance(self.parse('_cod_cell_volume<100.0'), Tree)
+        self.assertIsInstance(self.parse('_mp_bandgap > 5.0 AND _cod_molecular_weight < 350'), Tree)
+        self.assertIsInstance(self.parse('_cod_melting_point<300 AND nelements=4 AND elements="Si,O2"'), Tree)
+        self.assertIsInstance(self.parse('number=0.ANDnumber=.0ANDnumber=0.0ANDnumber=+0ANDNUMBER=-0AND'
+                                         'number=0e1ANDnumber=0e-1ANDnumber=0e+1'), Tree)
+        self.assertIsInstance(self.parse('key=value'), Tree)
+        self.assertIsInstance(self.parse('author=" someone "'), Tree)
+        self.assertIsInstance(self.parse('NOTICE=val'), Tree)
+        self.assertIsInstance(self.parse('author="Sąžininga Žąsis"'), Tree)
+        self.assertIsInstance(
+            self.parse('a = 12345 AND b = +12 AND c = -34 AND d = 1.2 AND e = .2E7 AND f = -.2E+7 AND '
+                       'g = +10.01E-10 AND h = 6.03e23 AND i = .1E1 AND j = -.1e1 AND k = 1.e-12 AND '
+                       'l = -.1e-12 AND m = 1000000000.E1000000000'), Tree)
+        self.assertIsInstance(self.parse('field = "!#$%&\'() * +, -./:; <= > ? @[] ^ `{|}~ % "'), Tree)
 
     def test_fails(self):
-        for tf in self.fails:
-            self.assertRaises(ParserError, self.parser.parse, tf)
+        with self.assertRaises(ParserError):
+            self.parse('number=0.0.1')
 
     def test_parser_version(self):
         self.assertEqual(self.parser.version, self.version)
@@ -108,7 +121,7 @@ class FilterParserTest(unittest.TestCase):
 
     def test_repr(self):
         self.assertIsNotNone(repr(self.parser))
-        self.parser.parse(self.passes[0])
+        self.parser.parse('')
         self.assertIsNotNone(repr(self.parser))
 
 
---
+++ optimade/filtertransformers/tests/test_mongo.py
@@ -0,0 +1,62 @@
+import unittest
+from optimade.filterparser import LarkParser
+from optimade.filtertransformers.mongo import NewMongoTransformer
+
+
+class TestMongoTransformer(unittest.TestCase):
+    version = (0, 10, 0)
+    variant = 'default'
+
+    def setUp(self):
+        p = LarkParser(version=self.version, variant=self.variant)
+        t = NewMongoTransformer()
+        self.transform = lambda inp: t.transform(p.parse(inp))
+
+    def test_simple_comparisons(self):
+        self.assertEqual(self.transform("a<3"), {"a": {"$lt": 3}})
+        self.assertEqual(self.transform("a<=3"), {"a": {"$lte": 3}})
+        self.assertEqual(self.transform("a>3"), {"a": {"$gt": 3}})
+        self.assertEqual(self.transform("a>=3"), {"a": {"$gte": 3}})
+        self.assertIn(self.transform("a=3"), [{"a": {"$eq": 3}}, {"a": 3}])
+        self.assertEqual(self.transform("a!=3"), {"a": {"$ne": 3}})
+
+    def test_not(self):
+        self.assertEqual(self.transform("NOT a<3"), {"a": {"$not": {"$lt": 3}}})
+
+    def test_conjunctions(self):
+        self.assertEqual(self.transform("a<5 AND b=0"),
+                         {'$and': [{'a': {'$lt': 5}}, {'b': {'$eq': 0}}]})
+        self.assertEqual(self.transform("a >= 8 OR a<5 AND b>=8"),
+                         {'$or': [{'a': {'$gte': 8}}, {'$and': [{'a': {'$lt': 5}}, {'b': {'$gte': 8}}]}]})
+
+    def test_should_pass(self):
+        self.assertEqual(self.transform('te < st'), {'te': {'$lt': 'st'}})
+        self.assertEqual(self.transform('spacegroup="P2"'), {'spacegroup': {'$eq': 'P2'}})
+        self.assertEqual(self.transform('_cod_cell_volume<100.0'), {'_cod_cell_volume': {'$lt': 100.0}})
+        self.assertEqual(
+            self.transform('_mp_bandgap > 5.0 AND _cod_molecular_weight < 350'),
+            {'$and': [{'_mp_bandgap': {'$gt': 5.0}}, {'_cod_molecular_weight': {'$lt': 350}}]})
+        self.assertEqual(
+            self.transform('_cod_melting_point<300 AND nelements=4 AND elements="Si,O2"'),
+            {'$and': [{'_cod_melting_point': {'$lt': 300}}, {'nelements': {'$eq': 4}}, {'elements': {'$eq': 'Si,O2'}}]})
+        self.assertEqual(
+            self.transform('number=0.ANDnumber=.0ANDnumber=0.0AND'
+                           'number=+0ANDNUMBER=-0ANDnumber=0e1AND'
+                           'number=0e-1ANDnumber=0e+1'),
+            {'$and': [{'number': {'$eq': 0.0}}, {'number': {'$eq': 0.0}}, {'number': {'$eq': 0.0}},
+                      {'number': {'$eq': 0}}, {'NUMBER': {'$eq': 0}}, {'number': {'$eq': 0.0}},
+                      {'number': {'$eq': 0.0}}, {'number': {'$eq': 0.0}}]})
+        self.assertEqual(self.transform('key=value'), {'key': {'$eq': 'value'}})
+        self.assertEqual(self.transform('author=" someone "'), {'author': {'$eq': ' someone '}})
+        self.assertEqual(self.transform('author="Sąžininga Žąsis"'), {'author': {'$eq': 'Sąžininga Žąsis'}})
+        self.assertEqual(
+            self.transform('a = 12345 AND b = +12 AND c = -34 AND d = 1.2 AND e = .2E7 AND f = -.2E+7 AND '
+                           'g = +10.01E-10 AND h = 6.03e23 AND i = .1E1 AND j = -.1e1 AND k = 1.e-12 AND '
+                           'l = -.1e-12 AND m = 1000000000.E1000000000'),
+            {'$and': [{'a': {'$eq': 12345}}, {'b': {'$eq': 12}}, {'c': {'$eq': -34}}, {'d': {'$eq': 1.2}},
+                      {'e': {'$eq': 2000000.0}}, {'f': {'$eq': -2000000.0}}, {'g': {'$eq': 1.001e-09}},
+                      {'h': {'$eq': 6.03e+23}}, {'i': {'$eq': 1.0}}, {'j': {'$eq': -1.0}},
+                      {'k': {'$eq': 1e-12}}, {'l': {'$eq': -1e-13}}, {'m': {'$eq': float('inf')}}]})
+        self.assertEqual(self.transform('field = "!#$%&\'() * +, -./:; <= > ? @[] ^ `{|}~ % "'),
+                         {'field': {'$eq': "!#$%&'() * +, -./:; <= > ? @[] ^ `{|}~ % "}})
+        self.assertEqual(self.transform('NOTICE=val'), {'ICE': {'$not': {'$eq': 'val'}}})
