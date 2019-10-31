6abcf1d21caab56649840fd80f6ef0c8391ef316
Matthew Evans <me388@cam.ac.uk>
2019-10-31 21:42:20 +0000

    Skip ElasticSearch tests if its not installed

--- optimade/filtertransformers/tests/test_elasticsearch.py
+++ optimade/filtertransformers/tests/test_elasticsearch.py
@@ -1,39 +1,54 @@
-from unittest import TestCase
+from unittest import TestCase, skipIf
 
 from optimade.filterparser import LarkParser
-from optimade.filtertransformers.elasticsearch import Transformer, Quantity
+
+try:
+    from optimade.filtertransformers.elasticsearch import Transformer, Quantity
+
+    ELASTICSEARCH_IMPORTED = True
+except ImportError:
+    ELASTICSEARCH_IMPORTED = False
 
 
+@skipIf(not ELASTICSEARCH_IMPORTED, "No ElasticSearch installation, skipping tests...")
 class TestTransformer(TestCase):
     def setUp(self):
         self.parser = LarkParser(version=(0, 10, 0))
 
-        nelements = Quantity(name='nelements')
-        elements_only = Quantity(name='elements_only')
-        elements_ratios = Quantity(name='elements_ratios')
+        nelements = Quantity(name="nelements")
+        elements_only = Quantity(name="elements_only")
+        elements_ratios = Quantity(name="elements_ratios")
         elements_ratios.nested_quantity = elements_ratios
         elements = Quantity(
-            name='elements', length_quantity=nelements, has_only_quantity=elements_only,
-            nested_quantity=elements_ratios)
-        dimension_types = Quantity(name='dimension_types')
+            name="elements",
+            length_quantity=nelements,
+            has_only_quantity=elements_only,
+            nested_quantity=elements_ratios,
+        )
+        dimension_types = Quantity(name="dimension_types")
         dimension_types.length_quantity = dimension_types
 
         quantities = [
-            nelements, elements_only, elements_ratios, elements, dimension_types,
-            Quantity(name='chemical_formula_reduced')]
+            nelements,
+            elements_only,
+            elements_ratios,
+            elements,
+            dimension_types,
+            Quantity(name="chemical_formula_reduced"),
+        ]
 
         self.transformer = Transformer(quantities=quantities)
 
     def test_parse_n_transform(self):
         queries = [
-            ('nelements > 1', 4),
-            ('nelements >= 2', 4),
-            ('nelements > 2', 1),
-            ('nelements < 4', 4),
-            ('nelements < 3', 3),
-            ('nelements <= 3', 4),
-            ('nelements != 2', 1),
-            ('1 < nelements', 4),
+            ("nelements > 1", 4),
+            ("nelements >= 2", 4),
+            ("nelements > 2", 1),
+            ("nelements < 4", 4),
+            ("nelements < 3", 3),
+            ("nelements <= 3", 4),
+            ("nelements != 2", 1),
+            ("1 < nelements", 4),
             ('elements HAS "H"', 4),
             ('elements HAS ALL "H", "O"', 4),
             ('elements HAS ALL "H", "C"', 1),
@@ -44,8 +59,8 @@ class TestTransformer(TestCase):
             ('elements:elements_ratios HAS "H":>0.66', 2),
             ('elements:elements_ratios HAS ALL "O":>0.33', 3),
             ('elements:elements_ratios HAS ALL "O":>0.33,"O":<0.34', 2),
-            ('elements IS KNOWN', 4),
-            ('elements IS UNKNOWN', 0),
+            ("elements IS KNOWN", 4),
+            ("elements IS UNKNOWN", 0),
             ('chemical_formula_reduced = "H2O"', 2),
             ('chemical_formula_reduced CONTAINS "H2"', 3),
             ('chemical_formula_reduced CONTAINS "H"', 4),
@@ -54,16 +69,17 @@ class TestTransformer(TestCase):
             ('chemical_formula_reduced STARTS WITH "H2"', 3),
             ('chemical_formula_reduced ENDS WITH "C"', 1),
             ('chemical_formula_reduced ENDS "C"', 1),
-            ('LENGTH elements = 2', 3),
-            ('LENGTH elements = 3', 1),
-            ('LENGTH dimension_types = 0', 3),
-            ('LENGTH dimension_types = 1', 1),
-            ('nelements = 2 AND LENGTH dimension_types = 1', 1),
-            ('nelements = 3 AND LENGTH dimension_types = 1', 0),
-            ('nelements = 3 OR LENGTH dimension_types = 1', 2),
-            ('nelements > 1 OR LENGTH dimension_types = 1 AND nelements = 2', 4),
-            ('(nelements > 1 OR LENGTH dimension_types = 1) AND nelements = 2', 3),
-            ('NOT LENGTH dimension_types = 1', 3)]
+            ("LENGTH elements = 2", 3),
+            ("LENGTH elements = 3", 1),
+            ("LENGTH dimension_types = 0", 3),
+            ("LENGTH dimension_types = 1", 1),
+            ("nelements = 2 AND LENGTH dimension_types = 1", 1),
+            ("nelements = 3 AND LENGTH dimension_types = 1", 0),
+            ("nelements = 3 OR LENGTH dimension_types = 1", 2),
+            ("nelements > 1 OR LENGTH dimension_types = 1 AND nelements = 2", 4),
+            ("(nelements > 1 OR LENGTH dimension_types = 1) AND nelements = 2", 3),
+            ("NOT LENGTH dimension_types = 1", 3),
+        ]
 
         for query, _ in queries:
             tree = self.parser.parse(query)
@@ -71,8 +87,9 @@ class TestTransformer(TestCase):
             assert result is not None
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import unittest
     import sys
+
     suite = unittest.TestLoader().loadTestsFromModule(sys.modules[__name__])
     unittest.TextTestRunner().run(suite)
