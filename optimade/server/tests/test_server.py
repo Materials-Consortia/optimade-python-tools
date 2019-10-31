8b2bdfe3b9538c0333d4a602abe25ebecf681f4e
Matthew Evans <me388@cam.ac.uk>
2019-10-31 21:29:03 +0000

    Added simple tests for the other endpoints: needs to be extended to actually test outcomes

--- optimade/server/tests/test_server.py
+++ optimade/server/tests/test_server.py
@@ -9,3 +9,15 @@ PREFIX = ""  # E.g. '', 'optimade/', 'optimade/v0.9', etc.
 
 def test_info_endpoint():
     client.get("/info")
+
+
+def test_info_structures_endpoint():
+    client.get("/info/structures")
+
+
+def test_structures_endpoint():
+    client.get("/structures")
+
+
+def test_single_structure_endpoint():
+    client.get("/structures/0")
