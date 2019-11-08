f1fee0c5eddcda70a057b82231defbcf5b8af9e1
Matthew Evans <me388@cam.ac.uk>
2019-11-08 15:27:59 +0000

    Improved CLI help and added --as_type flag

--- optimade/validator/__init__.py
+++ optimade/validator/__init__.py
@@ -1,3 +1,8 @@
+""" This module contains the ImplementationValidator class and corresponding
+command line tools.
+
+"""
+
 from .validator import ImplementationValidator
 
 __all__ = ["ImplementationValidator", "validate"]
@@ -8,7 +13,24 @@ def validate():
     import sys
     import traceback
 
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        prog="optimade_validator",
+        description="""Tests OPTiMaDe implementations for compliance with the optimade-python-tools models.
+
+    - To test an entire implementation (at say example.com/optimade) for all required/available endpoints:
+
+        $ optimade_validator http://example.com/optimade
+
+    - To test a particular response of an implementation against a particular model:
+
+        $ optimade_validator http://example.com/optimade/structures/id=1234 --as_type structure
+
+    - To test a particular response of an implementation against a particular model:
+
+        $ optimade_validator http://example.com/optimade/structures --as_type structures
+        """,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
     parser.add_argument(
         "base_url",
         nargs="?",
@@ -21,10 +43,23 @@ def validate():
     parser.add_argument(
         "--verbosity", "-v", type=int, default=0, help="The verbosity of the output"
     )
+    parser.add_argument(
+        "--as_type",
+        "-a",
+        type=str,
+        help=(
+            """ Validate the request URL with the provided type, rather than scanning the entire implementation e.g. optimade_validator `http://example.com/optimade/structures/0 --as_type structures`"""
+        ),
+    )
+
     args = vars(parser.parse_args())
 
+    valid_types = ["info", "info/structures", "structures", "structure"]
+    if args["as_type"] is not None and args["as_type"] not in valid_types:
+        sys.exit("{args['as_type']} is not a valid type, must be one of {valid_types}")
+
     validator = ImplementationValidator(
-        base_url=args["base_url"], verbosity=args["verbosity"]
+        base_url=args["base_url"], verbosity=args["verbosity"], as_type=args["as_type"]
     )
 
     try:
