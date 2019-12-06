68c98f0f67a4a5082b6918766ccabab6320e25a4
Casper Welzel Andersen <casper.andersen@epfl.ch>
2019-12-06 11:58:10 +0100

    Update repository according to new pre-commit

--- .ci/json_diff.sh
+++ .ci/json_diff.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 diff=$(jsondiff openapi.json local_openapi.json);
 
-if [ ! "$diff" = "{}" ]; then 
-    echo -e "Generated OpenAPI spec did not match committed version.\nRun 'invoke update_openapijson' and re-commit.\nDiff:\n$diff"; 
+if [ ! "$diff" = "{}" ]; then
+    echo -e "Generated OpenAPI spec did not match committed version.\nRun 'invoke update_openapijson' and re-commit.\nDiff:\n$diff";
 exit 1;
 fi
