359c1be172972861714c983d14e41f15a14db79a
Casper Welzel Andersen <casper.andersen@epfl.ch>
2019-12-06 13:24:46 +0100

    Update badges in README
    Add OPTiMaDe version badge using shields.io's endpoint service. One can
    pass a URL to a JSON file, which will determine the badge.
    The URL points to a local repository file on the `master` branch.
    The local repository JSON file is checked during pre-commit, matching
    against the version in config.ini.
    
    A new task `set-optimade-ver` has been implemented, to update the
    OPTiMaDe version (updates the JSON file and config.ini).

---
+++ .ci/optimade_version_update.py
@@ -0,0 +1,52 @@
+import json
+from configparser import ConfigParser
+from pathlib import Path
+
+shields_json = Path(__file__).resolve().parent.joinpath("optimade-version.json")
+config_ini = (
+    Path(__file__).resolve().parent.parent.joinpath("optimade/server/config.ini")
+)
+
+with open(shields_json, "r") as fp:
+    shield = json.load(fp)
+
+config = ConfigParser()
+config.read(config_ini)
+
+shield_version = shield["message"]
+current_version = f'v{config.get("IMPLEMENTATION", "VERSION")}'
+
+if shield_version == current_version:
+    # The shield has the newest implemented version
+    print(
+        f"""They are the same: {current_version}
+Shield file:
+{json.dumps(shield, indent=2)}"""
+    )
+    exit(0)
+
+print(
+    f"""The shield version is outdated.
+Shield version: {shield_version}
+Current version: {current_version}
+"""
+)
+
+shield["message"] = current_version
+with open(shields_json, "w") as fp:
+    json.dump(shield, fp, indent=2)
+
+# Check file was saved correctly
+with open(shields_json, "r") as fp:
+    updated_shield = json.load(fp)
+
+if updated_shield["message"] == current_version:
+    print(f"Successfully updated the shield version to '{updated_shield['message']}'")
+    exit(0)
+else:
+    print(
+        f"""Something went wrong !
+Shield file:
+{json.dumps(updated_shield, indent=2)}"""
+    )
+    exit(1)
