b411bcc87b2c40648e4278d5c0a02c318bd7f093
Casper Welzel Andersen <casper.andersen@epfl.ch>
2019-12-11 17:18:23 +0100

    Update with new comment for index openapi

--- .ci/json_diff.sh
+++ .ci/json_diff.sh
@@ -5,12 +5,12 @@ python -c "from optimade.server.main_index import app, update_schema; update_sch
 diff=$(jsondiff openapi.json local_openapi.json);
 index_diff=$(jsondiff index_openapi.json local_index_openapi.json);
 
-if [ ! "$diff" = "{}" ]; then 
-    echo -e "Generated OpenAPI spec for test server did not match committed version.\nRun 'invoke update_openapijson' and re-commit.\nDiff:\n$diff"; 
+if [ ! "$diff" = "{}" ]; then
+    echo -e "Generated OpenAPI spec for test server did not match committed version.\nRun 'invoke update_openapijson' and re-commit.\nDiff:\n$diff";
 exit 1;
 fi
 
-if [ ! "$index_diff" = "{}" ]; then 
-    echo -e "Generated OpenAPI spec for Index meta-database did not match committed version. Diff:\n$index_diff"; 
+if [ ! "$index_diff" = "{}" ]; then
+    echo -e "Generated OpenAPI spec for Index meta-database did not match committed version.\nRun 'invoke update_openapijson' and re-commit.\nDiff:\n$index_diff";
 exit 1;
 fi
