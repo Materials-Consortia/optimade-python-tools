e9d1c98f50df29f015c913c40cb62a387244cb7d
Matthew Evans <me388@cam.ac.uk>
2020-01-18 14:04:48 +0000

    Added equivalent test for ini

--- tests/server/test_config.py
+++ tests/server/test_config.py
@@ -4,15 +4,33 @@ import unittest
 from pathlib import Path
 
 
+class LoadFromIniTest(unittest.TestCase):
+    """Test server.config.ServerConfig.load_from_ini"""
+
+    def test_config_ini(self):
+        """Invoke CONFIG using config_test.ini"""
+        from optimade.server.config import ServerConfig
+
+        CONFIG = ServerConfig(
+            server_cfg=Path(__file__).parent.joinpath("server_test_ini.cfg").resolve()
+        )
+
+        CONFIG.default_db  # Initiate CONFIG, running load_from_json()
+        # _path should now be updated with the correct path to the config json file:
+        self.assertEqual(
+            CONFIG._path, Path(__file__).parent.joinpath("config_test.ini").resolve()
+        )
+
+
 class LoadFromJsonTest(unittest.TestCase):
     """Test server.config.ServerConfig.load_from_json"""
 
     def test_config_json(self):
-        """Invoke CONFIG using config.json"""
+        """Invoke CONFIG using config_test.json"""
         from optimade.server.config import ServerConfig
 
         CONFIG = ServerConfig(
-            server_cfg=Path(__file__).parent.joinpath("server_test.cfg").resolve()
+            server_cfg=Path(__file__).parent.joinpath("server_test_json.cfg").resolve()
         )
 
         CONFIG.default_db  # Initiate CONFIG, running load_from_json()
