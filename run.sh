8d39011a0fe79a036670a7562f9a42383c2fa05e
Casper Welzel Andersen <casper.andersen@epfl.ch>
2020-02-07 00:41:56 +0100

    Reintroduce running run.sh index
    There are now two ways to start the index meta-database server, instead
    of the regular server.
    1. Run: ./run.sh index
    2. Set variable: export MAIN="main_index"
       And then run normally.
    The two methods are also prioritized this way, i.e., if the environment
    variable MAIN="main", but one runs ./run.sh index, then the index
    meta-database will be started instead of the regular server.

--- run.sh
+++ run.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -ex
 
-if [ "${MAIN}" == "main_index" ]; then
+if [ "$1" == "index" ]; then
+    MAIN="main_index"
     PORT=5001
 else
-    MAIN="main"
-    PORT=5000
+    if [ "${MAIN}" == "main_index" ]; then
+        PORT=5001
+    else
+        MAIN="main"
+        PORT=5000
+    fi
 fi
 
-uvicorn optimade.server.$MAIN:app --reload --host 0.0.0.0 --port $PORT
+uvicorn optimade.server.$MAIN:app --reload --port $PORT
