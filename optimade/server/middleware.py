f438e2618a751c212f31efefc314453dc27b549b
Matthew Evans <me388@cam.ac.uk>
2020-02-07 16:35:21 +0000

    Only redirect entry endpoints with middleware

--- optimade/server/middleware.py
+++ optimade/server/middleware.py
@@ -3,6 +3,8 @@ from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.requests import Request
 from starlette.responses import RedirectResponse
 
+from .routers import ENTRY_COLLECTIONS
+
 
 class RedirectSlashedURLs(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
@@ -10,17 +12,14 @@ class RedirectSlashedURLs(BaseHTTPMiddleware):
 
         E.g., `http://example.org/optimade/v0/info/` -> `http://example.org/optimade/v0/info`
         """
-        if request.scope["path"].endswith("/"):
+        if request.scope["path"].endswith("/") and any(
+            request.scope["path"].endswith(f"{endpoint}/")
+            for endpoint in list(ENTRY_COLLECTIONS.keys())
+        ):
             redirect_scope = dict(request.scope)
+            redirect_scope["path"] = redirect_scope["path"][:-1]
+            redirect_url = URL(scope=redirect_scope)
+            return RedirectResponse(url=str(redirect_url))
 
-            # Make sure to only redirect paths that are beyond the versioned depth
-            # e.g. don't redirect `/optimade/` to `/optimade` as this will loop indefinitely
-            if (
-                len([substr for substr in redirect_scope["path"].split("/") if substr])
-                > 1
-            ):
-                redirect_scope["path"] = redirect_scope["path"][:-1]
-                redirect_url = URL(scope=redirect_scope)
-                return RedirectResponse(url=str(redirect_url))
         response = await call_next(request)
         return response
