24f772b720908352ef01d5e37ab3402021818313
Matthew Evans <me388@cam.ac.uk>
2020-02-07 13:02:46 +0000

    Tweak middleware to only redirect urls of certain depth

--- optimade/server/middleware.py
+++ optimade/server/middleware.py
@@ -8,13 +8,14 @@ class RedirectSlashedURLs(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
         """Redirect URL requests ending with a slash to non-slashed URLs
 
-        E.g., `http://example.org/info/` -> `http://example.org/info`
+        E.g., `http://example.org/optimade/v0/info/` -> `http://example.org/optimade/v0/info`
         """
         if request.scope["path"].endswith("/"):
             redirect_scope = dict(request.scope)
 
-            # Make sure we're not dealing with a URL path (after the domain) of `/`
-            if redirect_scope["path"] != "/":
+            # Make sure to only redirect paths that are beyond the versioned depth
+            # e.g. don't redirect `/optimade/` to `/optimade` as this will loop indefinitely
+            if len([substr for substr in redirect_scope["path"].split('/') if substr]) > 1:
                 redirect_scope["path"] = redirect_scope["path"][:-1]
                 redirect_url = URL(scope=redirect_scope)
                 return RedirectResponse(url=str(redirect_url))
