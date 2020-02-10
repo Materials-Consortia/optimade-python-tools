7e8e9a5ec9d6c5003d8f5e177735a479f5b88b33
Matthew Evans <me388@cam.ac.uk>
2020-02-10 13:46:52 +0000

    Move default StructureMapper aliases to default config file

--- optimade/server/mappers/structures.py
+++ optimade/server/mappers/structures.py
@@ -6,9 +6,3 @@ __all__ = ("StructureMapper",)
 class StructureMapper(ResourceMapper):
 
     ENDPOINT = "structures"
-    ALIASES = (
-        ("id", "task_id"),
-        ("chemical_formula_descriptive", "pretty_formula"),
-        ("chemical_formula_reduced", "pretty_formula"),
-        ("chemical_formula_anonymous", "formula_anonymous"),
-    )
