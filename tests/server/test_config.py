5a0f9e62b31acdad694a98b9e6c797142d103065
Casper Welzel Andersen <casper.andersen@epfl.ch>
2020-02-19 17:12:14 +0100

    Add tests for debug mode

--- tests/server/test_config.py
+++ tests/server/test_config.py
@@ -1,7 +1,12 @@
-# pylint: disable=import-outside-toplevel,protected-access,pointless-statement
+# pylint: disable=protected-access,pointless-statement,relative-beyond-top-level
+import os
 import unittest
 
 from pathlib import Path
+
+from optimade.server.config import ServerConfig, CONFIG
+
+from .utils import SetClient, get_regular_client, get_index_client
 
 
 class LoadFromIniTest(unittest.TestCase):
@@ -9,8 +14,6 @@ class LoadFromIniTest(unittest.TestCase):
 
     def test_config_ini(self):
         """Invoke CONFIG using config_test.ini"""
-        from optimade.server.config import ServerConfig
-
         CONFIG = ServerConfig(
             server_cfg=Path(__file__).parent.joinpath("server_test_ini.cfg").resolve()
         )
@@ -27,8 +30,6 @@ class LoadFromJsonTest(unittest.TestCase):
 
     def test_config_json(self):
         """Invoke CONFIG using config_test.json"""
-        from optimade.server.config import ServerConfig
-
         CONFIG = ServerConfig(
             server_cfg=Path(__file__).parent.joinpath("server_test_json.cfg").resolve()
         )
@@ -38,3 +39,113 @@ class LoadFromJsonTest(unittest.TestCase):
         self.assertEqual(
             CONFIG._path, Path(__file__).parent.joinpath("config_test.json").resolve()
         )
+
+
+class TestDebug(unittest.TestCase):
+    """Test if debug mode is correctly set and respected"""
+
+    def test_env_variable(self):
+        """Set DEBUG environment variable and check CONFIG picks up on it correctly"""
+        os.environ["DEBUG"] = "1"
+        CONFIG = ServerConfig()
+        self.assertTrue(CONFIG.debug)
+
+        os.environ.pop("DEBUG", None)
+        CONFIG = ServerConfig()
+        self.assertFalse(CONFIG.debug)
+
+
+class TestDebugOff(SetClient, unittest.TestCase):
+
+    server = "regular"
+
+    def test_debug_is_respected_when_off(self):
+        """Make sure traceback is toggleable according to debug mode - here OFF
+
+        TODO: This should be moved to a separate test file that tests the exception handlers.
+        """
+        response = self.client.get("/non/existent/path")
+        self.assertEqual(
+            response.status_code,
+            404,
+            msg=f"Request should have failed, but didn't: {response.json()}",
+        )
+
+        response = response.json()
+        self.assertNotIn("data", response)
+        self.assertIn("meta", response)
+
+        self.assertNotIn(f"{CONFIG.provider['prefix']}traceback", response["meta"])
+
+
+class IndexTestDebugOff(SetClient, unittest.TestCase):
+
+    server = "index"
+
+    def test_debug_is_respected_when_off(self):
+        """Make sure traceback is toggleable according to debug mode - here OFF
+
+        TODO: This should be moved to a separate test file that tests the exception handlers.
+        """
+        response = self.client.get("/non/existent/path")
+        self.assertEqual(
+            response.status_code,
+            404,
+            msg=f"Request should have failed, but didn't: {response.json()}",
+        )
+
+        response = response.json()
+        self.assertNotIn("data", response)
+        self.assertIn("meta", response)
+
+        self.assertNotIn(f"{CONFIG.provider['prefix']}traceback", response["meta"])
+
+
+class TestDebugOn(unittest.TestCase):
+    def __init__(self, methodName: str = "runTest"):
+        super().__init__(methodName=methodName)
+        os.environ["DEBUG"] = "1"
+        self.client = get_regular_client()
+
+    def test_debug_is_respected_when_off(self):
+        """Make sure traceback is toggleable according to debug mode - here OFF
+
+        TODO: This should be moved to a separate test file that tests the exception handlers.
+        """
+        response = self.client.get("/non/existent/path")
+        self.assertEqual(
+            response.status_code,
+            404,
+            msg=f"Request should have failed, but didn't: {response.json()}",
+        )
+
+        response = response.json()
+        self.assertNotIn("data", response)
+        self.assertIn("meta", response)
+
+        self.assertNotIn(f"{CONFIG.provider['prefix']}traceback", response["meta"])
+
+
+class IndexTestDebugOn(unittest.TestCase):
+    def __init__(self, methodName: str = "runTest"):
+        super().__init__(methodName=methodName)
+        os.environ["DEBUG"] = "1"
+        self.client = get_index_client()
+
+    def test_debug_is_respected_when_off(self):
+        """Make sure traceback is toggleable according to debug mode - here OFF
+
+        TODO: This should be moved to a separate test file that tests the exception handlers.
+        """
+        response = self.client.get("/non/existent/path")
+        self.assertEqual(
+            response.status_code,
+            404,
+            msg=f"Request should have failed, but didn't: {response.json()}",
+        )
+
+        response = response.json()
+        self.assertNotIn("data", response)
+        self.assertIn("meta", response)
+
+        self.assertNotIn(f"{CONFIG.provider['prefix']}traceback", response["meta"])
