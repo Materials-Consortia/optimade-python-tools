1d4e7a5ac4b30048c820802778268a5d7e15a9c4
Casper Welzel Andersen <casper.andersen@epfl.ch>
2020-02-24 11:38:09 +0100

    GitHub Action for OPTiMaDe validator
    Update README and invoke tasks.
    
    The action runs the `optimade_validator` in a docker, with support for
    validating "local" running servers within the GitHub Action runner, as
    well as external, deployed servers.

--- tasks.py
+++ tasks.py
@@ -1,8 +1,19 @@
 import re
+from typing import Tuple
 
 from invoke import task
 
 from optimade import __version__
+
+
+def update_file(filename: str, sub_line: Tuple[str, str], strip: str = None):
+    """Utility function for tasks to read, update, and write files"""
+    with open(filename, "r") as handle:
+        lines = [re.sub(sub_line[0], sub_line[1], l.rstrip(strip)) for l in handle]
+
+    with open(filename, "w") as handle:
+        handle.write("\n".join(lines))
+        handle.write("\n")
 
 
 @task
@@ -15,23 +26,11 @@ def setver(_, patch=False, new_ver=""):
         v = [int(x) for x in __version__.split(".")]
         v[2] += 1
         new_ver = ".".join(map(str, v))
-    with open("optimade/__init__.py", "r") as f:
-        lines = [
-            re.sub("__version__ = .+", '__version__ = "{}"'.format(new_ver), l.rstrip())
-            for l in f
-        ]
-    with open("optimade/__init__.py", "w") as f:
-        f.write("\n".join(lines))
-        f.write("\n")
 
-    with open("setup.py", "r") as f:
-        lines = [
-            re.sub("version=([^,]+),", 'version="{}",'.format(new_ver), l.rstrip())
-            for l in f
-        ]
-    with open("setup.py", "w") as f:
-        f.write("\n".join(lines))
-        f.write("\n")
+    update_file(
+        "optimade/__init__.py", ("__version__ = .+", f'__version__ = "{new_ver}"')
+    )
+    update_file("setup.py", ("version=([^,]+),", f'version="{new_ver}",'))
 
     print("Bumped version to {}".format(new_ver))
 
@@ -59,36 +58,35 @@ def set_optimade_ver(_, ver=""):
     if not re.match("[0-9]+.[0-9]+.[0-9]+", ver):
         raise Exception("ver MUST be expressed as 'Major.Minor.Patch'")
 
-    with open("optimade/__init__.py", "r") as f:
-        lines = [
-            re.sub("__api_version__ = .+", f'__api_version__ = "{ver}"', l.rstrip())
-            for l in f
-        ]
-    with open("optimade/__init__.py", "w") as f:
-        f.write("\n".join(lines))
-        f.write("\n")
-
-    with open(".ci/optimade-version.json", "r") as f:
-        lines = [
-            re.sub('"message": .+', f'"message": "v{ver}",', l.rstrip()) for l in f
-        ]
-    with open(".ci/optimade-version.json", "w") as f:
-        f.write("\n".join(lines))
-        f.write("\n")
-
-    with open(".github/workflows/deps_lint.yml", "r") as f:
-        lines = f
-        for regex, version in (
-            ("[0-9]+", ver.split(".")[0]),
-            ("[0-9]+.[0-9]+", ".".join(ver.split(".")[:2])),
-            ("[0-9]+.[0-9]+.[0-9]+", ver),
-        ):
-            lines = [
-                re.sub(f"/optimade/v{regex}", f"/optimade/v{version}", l.rstrip())
-                for l in lines
-            ]
-    with open(".github/workflows/deps_lint.yml", "w") as f:
-        f.write("\n".join(lines))
-        f.write("\n")
+    update_file(
+        "optimade/__init__.py", ("__api_version__ = .+", f'__api_version__ = "{ver}"')
+    )
+    update_file(".ci/optimade-version.json", ('"message": .+', f'"message": "v{ver}",'))
+    for regex, version in (
+        ("[0-9]+", ver.split(".")[0]),
+        ("[0-9]+.[0-9]+", ".".join(ver.split(".")[:2])),
+        ("[0-9]+.[0-9]+.[0-9]+", ver),
+    ):
+        update_file(
+            ".github/workflows/deps_lint.yml",
+            (f"/optimade/v{regex}", f"/optimade/v{version}"),
+        )
+        update_file(
+            "README.md", (f"example/v{regex}", f"example/v{version}"), strip="\n"
+        )
+    update_file(
+        ".github/workflows/validator_action.yml",
+        ("/optimade/v[0-9]+", f"/optimade/v{ver.split('.')[0]}"),
+    )
+    update_file(
+        "README.md", ("optimade/v[0-9]+", f"optimade/v{ver.split('.')[0]}"), strip="\n"
+    )
+    update_file(
+        "optimade/validator/github_action/entrypoint.sh",
+        (
+            "'[0-9]+' '[0-9]+.[0-9]+' '[0-9]+.[0-9]+.[0-9]+'",
+            f"'{ver.split('.')[0]}' '{'.'.join(ver.split('.')[:2])}' '{ver}'",
+        ),
+    )
 
     print(f"Bumped OPTiMaDe version to {ver}")
