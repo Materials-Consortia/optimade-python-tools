41031800da0b4b59a313c412d16f3c5fbd66e03f
Shyam Dwaraknath <shyamd@lbl.gov>
2020-03-05 14:01:05 -0800

    one last attempt to fix this validator

--- .ci/index_openapi_diff.sh
+++ .ci/index_openapi_diff.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Note: This returns exit code 0, if the two specs agree, otherwise exit code 1
-docker run -t -v $1:/specs:ro quen2404/openapi-diff /specs/openapi/index_openapi.json /specs/openapi/local_index_openapi.json
+docker run -t -v $(pwd)/src:/specs:ro quen2404/openapi-diff /specs/openapi/index_openapi.json /specs/openapi/local_index_openapi.json
