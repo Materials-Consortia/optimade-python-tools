f1fec914e6db4ddbd760b2e660c333d913ceda30
Shyam Dwaraknath <shyamd@lbl.gov>
2020-03-12 15:45:23 -0700

    add providers and aliases

--- optimade/server/config.py
+++ optimade/server/config.py
@@ -1,11 +1,12 @@
 import os
 import json
-from typing import Any, Optional
+from typing import Any, Optional, Dict, List
+from typing_extensions import Literal
 from configparser import ConfigParser
 from pathlib import Path
 from warnings import warn
 
-from pydantic import BaseSettings
+from pydantic import BaseSettings, root_validator
 
 from optimade import __version__
 from optimade.models import Implementation, Provider
@@ -20,7 +21,7 @@ class ServerConfig(BaseSettings):
     can be easily extended for new config file types.
 
     """
-
+    debug: bool = False
     use_real_mongo: bool = False
     mongo_database: str = "optimade"
     mongo_uri: str = "localhost:27017"
@@ -31,18 +32,27 @@ class ServerConfig(BaseSettings):
     page_limit_max: int = 500
     default_db: str = "test_server"
     base_url: Optional[str] = None
-    implementation: Implementation = Implementation()
-    provider: Provider = Provider()
+    implementation: Implementation = Implementation(
+        name="Example implementation",
+        version=__version__,
+        source_url="https://github.com/Materials-Consortia/optimade-python-tools",
+        maintainer=None,
+    )
+    provider: Provider = Provider(
+        prefix="_exmpl_",
+        name="Example provider",
+        description="Provider used for examples, not to be assigned to a real database",
+        homepage="https://example.com",
+        index_base_url=None,
+    )
+    provider_fields: Dict[Literal["links", "references", "structures"], List[str]] = {}
+    aliases: Dict[Literal["links", "references", "structures"], Dict[str, str]] = {}
 
     index_links_path: Path = Path(__file__).parent.joinpath("index_links.json")
 
     class Config:
-        env_file = ".optimade"
+        env_file = ".optimade.json"
         env_prefix = "optimade_"
-        fields = {
-            "auth_key": {"env": "my_auth_key"},
-            "redis_dsn": {"env": ["service_redis_dsn", "redis_url"]},
-        }
 
 
 CONFIG = ServerConfig()
