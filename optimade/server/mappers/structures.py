3f9070513d16a5216081f9424aa4363afdf99222
Matthew Evans <me388@cam.ac.uk>
2020-03-24 17:13:09 +0000

    Added spec-defined length aliases to StructureMapper

--- optimade/server/mappers/structures.py
+++ optimade/server/mappers/structures.py
@@ -6,3 +6,9 @@ __all__ = ("StructureMapper",)
 class StructureMapper(BaseResourceMapper):
 
     ENDPOINT = "structures"
+
+    LENGTH_ALIASES = (
+        ("elements", "nelements"),
+        ("cartesian_site_positions", "nsites"),
+        ("species_at_sites", "nsites"),
+    )
