7768f62f2530c8310ae77fe92556a91f2dfe91af
Shyam Dwaraknath <shyamd@lbl.gov>
2020-03-25 10:47:43 -0700

    Fixes Local Tests (#240)
    Force tests to use `test_config.json`

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+import os
+import pytest
+from pathlib import Path
+
+
+def pytest_configure(config):
+    """
+    Method that runs before pytest collects tests so no modules
+    are imported
+    """
+    cwd = Path(__file__).parent
+    os.environ["OPTIMADE_CONFIG_FILE"] = str(cwd / "test_config.json")
