9ed2afa181788e0257bfed178ac363d7e613aba1
Shyam Dwaraknath <shyamd@lbl.gov>
2020-05-04 10:55:07 -0700

    break up main package into namespace backages

---
+++ optimade-server/setup.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+from setuptools import setup, find_namespace_packages
+
+
+module_dir = Path(__file__).resolve().parent
+
+with open(module_dir.joinpath("requirements.txt")) as f:
+    requirements = f.read()
+
+setup(
+    name="optimade-server",
+    url="https://github.com/Materials-Consortia/optimade-python-tools",
+    license="MIT",
+    author="OPTIMADE Development Team",
+    author_email="dev@optimade.org",
+    description="Python server implementation for the OPTIMADE APIs.",
+    version="0.8.1",
+    keywords="optimade jsonapi materials",
+    packages=find_namespace_packages(include=["optimade.*"]),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Intended Audience :: Developers",
+        "Topic :: Database",
+        "Topic :: Database :: Database Engines/Servers",
+        "Topic :: Database :: Front-Ends",
+    ],
+    package_data={"": ["*.json", "*.html"]},
+    python_requires=">=3.6",
+    install_requires=requirements,
+)
