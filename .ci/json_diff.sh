7073d621b7d391c84649556f2060532d515a1242
Shyam Dwaraknath <shyamd@lbl.gov>
2020-05-05 15:08:02 -0700

    fix json diff

--- .ci/json_diff.sh
+++ .ci/json_diff.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-python -c "from optimade.server.main import app, update_schema; update_schema(app)"
-python -c "from optimade.server.main_index import app, update_schema; update_schema(app)"
+invoke generate-openapi
 
 diff=$(jsondiff ./openapi/openapi.json ./openapi/local_openapi.json);
 index_diff=$(jsondiff ./openapi/index_openapi.json ./openapi/local_index_openapi.json);
 
-if [ ! "$diff" = "{}" ]; then
+if [ ! "$diff" = "" ]; then
     echo -e "Generated OpenAPI spec for test server did not match committed version.\nRun 'invoke update-openapijson' and re-commit.\nDiff:\n$diff";
 exit 1;
 fi
 
-if [ ! "$index_diff" = "{}" ]; then
+if [ ! "$index_diff" = "" ]; then
     echo -e "Generated OpenAPI spec for Index meta-database did not match committed version.\nRun 'invoke update-openapijson' and re-commit.\nDiff:\n$index_diff";
 exit 1;
 fi
