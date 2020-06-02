64c89634bb79601537bcba55da1067765a3a9b40
Matthew Evans <me388@cam.ac.uk>
2020-06-02 16:43:02 +0100

    Add some extra aliasing tests for the mapper

--- tests/server/test_mappers.py
+++ tests/server/test_mappers.py
@@ -16,3 +16,36 @@ class ResourceMapperTests(unittest.TestCase):
         toy_collection = mongomock.MongoClient()["fake"]["fake"]
         with self.assertRaises(RuntimeError):
             MongoCollection(toy_collection, StructureResource, mapper)
+
+    def test_property_aliases(self):
+        class MyMapper(BaseResourceMapper):
+            PROVIDER_FIELDS = ("dft_parameters", "test_field")
+            LENGTH_ALIASES = (("_exmpl_test_field", "test_field_len"),)
+            ALIASES = (("field", "completely_different_field"),)
+
+        mapper = MyMapper()
+        self.assertEqual(mapper.alias_for("_exmpl_dft_parameters"), "dft_parameters")
+        self.assertEqual(mapper.alias_for("_exmpl_test_field"), "test_field")
+        self.assertEqual(mapper.alias_for("field"), "completely_different_field")
+        self.assertEqual(mapper.length_alias_for("_exmpl_test_field"), "test_field_len")
+        self.assertEqual(mapper.length_alias_for("test_field"), None)
+        self.assertEqual(mapper.alias_for("test_field"), "test_field")
+
+        # nested properties
+        self.assertEqual(
+            mapper.alias_for("_exmpl_dft_parameters.nested.property"),
+            "dft_parameters.nested.property",
+        )
+        self.assertEqual(
+            mapper.alias_for("_exmpl_dft_parameters.nested_property"),
+            "dft_parameters.nested_property",
+        )
+
+        # test nonsensical query
+        self.assertEqual(mapper.alias_for("_exmpl_test_field."), "test_field.")
+
+        # test an awkward case that has no alias
+        self.assertEqual(
+            mapper.alias_for("_exmpl_dft_parameters_dft_parameters.nested.property"),
+            "_exmpl_dft_parameters_dft_parameters.nested.property",
+        )
