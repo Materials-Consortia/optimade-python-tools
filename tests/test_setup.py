a4805bcb1b696909f184a16d76809fbac13720b0
Shyam D <shyamd@lbl.gov>
2020-06-10 14:29:37 -0700

    parameterize test for more visibility

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -2,59 +2,38 @@ import os
 from pathlib import Path
 import re
 import tempfile
-import unittest
+import pytest
 
 
-class TestSetup(unittest.TestCase):
-    """Test ./setup.py"""
+package_data = {
+    "optimade-core": [r"\.lark"],
+    "optimade-server": [
+        r"index_links\.json",
+        r"test_structures\.json",
+        r"test_references\.json",
+        r"test_links\.json",
+        r"landing_page\.html",
+    ],
+    "optimade-validator": [r"filters\.txt", r"optional_filters\.txt"],
+}
 
-    def test_distributions_package(self):
-        """Make sure a distribution has all the needed package data"""
-        package_root = Path(__file__).parent.parent.resolve()
 
-        number_of_grammar_files = len(
-            list(package_root.joinpath("optimade/grammar").rglob("*.lark"))
-        )
+@pytest.mark.parametrize("ensure_package_data", list(package_data.items()))
+def test_distribution_package_data(ensure_package_data):
+    """Make sure a distribution has all the needed package data"""
+    package_name, files = ensure_package_data
+    repo_root = Path(__file__).parent.parent.resolve()
+    package_root = repo_root.joinpath(package_name)
+    print(package_root)
 
-        with tempfile.TemporaryDirectory() as temp_dir:
-            current_dir = Path(temp_dir)
-            file_output = current_dir.joinpath("sdist.out")
+    with tempfile.TemporaryDirectory() as temp_dir:
+        file_output = Path(temp_dir).joinpath("sdist.out")
 
-            os.system(
-                f"python {package_root.joinpath('setup.py')} sdist --dry-run > {file_output}"
-            )
+        os.chdir(package_root)
+        os.system(f"python setup.py sdist --dry-run > {file_output}")
 
-            with open(file_output, "r") as file_:
-                # Removing new-line-chars via slice
-                lines = [_[:-1] for _ in file_.readlines()]
+        with open(file_output, "r") as file_:
+            lines = file_.read()
 
-            present = {
-                r"\.lark": False,
-                r"index_links\.json": False,
-                r"test_structures\.json": False,
-                r"test_references\.json": False,
-                r"test_links\.json": False,
-                r"filters\.txt": False,
-                r"optional_filters\.txt": False,
-                r"landing_page\.html": False,
-            }
-            count = 0
-            for line in lines:
-                for requirement in present:
-                    if re.findall(requirement, line):
-                        if requirement == r"\.lark":
-                            count += 1
-                            if count == number_of_grammar_files:
-                                present[requirement] = True
-                            else:
-                                present[requirement] = False
-                        else:
-                            present[requirement] = True
-
-            for requirement, is_present in present.items():
-                if requirement == r"\.lark":
-                    msg = f"{requirement} files NOT found. Expected {number_of_grammar_files}, found instead {count}."
-                else:
-                    msg = f"{requirement} file NOT found."
-
-                self.assertTrue(is_present, msg=msg)
+        for requirement in files:
+            assert re.findall(requirement, lines), f"{requirement} file NOT found."
