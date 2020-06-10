bb5f1edd0f0068a73d0cbb24b8c60d0d8251eeda
Shyam D <shyamd@lbl.gov>
2020-06-10 14:46:42 -0700

    fix setup tests

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -5,26 +5,24 @@ import tempfile
 import pytest
 
 
-package_data = {
-    "optimade-core": [r"\.lark"],
-    "optimade-server": [
-        r"index_links\.json",
-        r"test_structures\.json",
-        r"test_references\.json",
-        r"test_links\.json",
-        r"landing_page\.html",
-    ],
-    "optimade-validator": [r"filters\.txt", r"optional_filters\.txt"],
-}
+package_data = [
+    r"\.lark",
+    r"index_links\.json",
+    r"test_structures\.json",
+    r"test_references\.json",
+    r"test_links\.json",
+    r"landing_page\.html",
+    r"filters\.txt",
+    r"optional_filters\.txt",
+]
 
 
-@pytest.mark.parametrize("ensure_package_data", list(package_data.items()))
+@pytest.mark.parametrize("ensure_package_data", list(package_data))
 def test_distribution_package_data(ensure_package_data):
     """Make sure a distribution has all the needed package data"""
-    package_name, files = ensure_package_data
+    files = ensure_package_data
     repo_root = Path(__file__).parent.parent.resolve()
-    package_root = repo_root.joinpath(package_name)
-    print(package_root)
+    package_root = repo_root.joinpath("optimade")
 
     with tempfile.TemporaryDirectory() as temp_dir:
         file_output = Path(temp_dir).joinpath("sdist.out")
