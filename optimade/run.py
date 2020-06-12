250f20c3ebef875fcbb6ffc743af29ecf0a9af3d
Adam Fekete <adam.fekete@kcl.ac.uk>
2020-06-12 13:07:02 +0200

    Revert "simple run script"
    This reverts commit be037a854f6bc353c0b93195b918a605392fef01.

--- optimade/run.py
+++
@@ -1,9 +0,0 @@
-from optimade.server.main import app
-from optimade.server.config import CONFIG
-
-if __name__ == "__main__":
-    import uvicorn
-
-    CONFIG.debug = True
-
-    uvicorn.run(app, host="0.0.0.0", port=5000)
