fce2f45fb805e39fb697e0e768ad5ef15d7ee459
Casper Welzel Andersen <casper.andersen@epfl.ch>
2020-06-26 02:42:14 +0200

    Update to test updating tags

--- .github/workflows/update_changelog.sh
+++ .github/workflows/update_changelog.sh
@@ -1,22 +1,21 @@
 #!/bin/sh
 set -e
 
-echo "-o- Checkout fresh branch -o-"
+echo "\n-o- Checkout fresh branch -o-"
 git checkout -b update_changelog
 
-echo "-o- Setting commit user -o-"
+echo "\n-o- Setting commit user -o-"
 git config --local user.email "dev@optimade.org"
 git config --local user.name "OPTIMADE Developers"
 
-echo "-o- Install current SHA (optimade) -o-"
-pip install -U -e .[dev]
-pip install -r requirements-docs.txt
+echo "\n-o- Install current SHA (optimade) -o-"
+pip install -U -e .[server]
+pip install invoke jsondiff
 
 echo "-o- Update version -o-"
-# invoke setver -v ${GITHUB_REF#refs/tags/}
-invoke setver -v "0.10.0"
+invoke setver -v ${GITHUB_REF#refs/tags/}
 
-echo "-o- Generate changelog -o-"
+echo "\n-o- Generate changelog -o-"
 apt-get update
 apt-get -y install ruby-full
 export GEM_HOME="${HOME}/gems"
@@ -24,12 +23,11 @@ export PATH="${GEM_HOME}/bin:${PATH}"
 gem install github_changelog_generator
 github_changelog_generator --user "Materials-Consortia" --project "optimade-python-tools" --token ${INPUT_TOKEN}
 
-echo "-o- Overwrite old CHANGELOG.md -o-"
+echo "\n-o- Overwrite old CHANGELOG.md -o-"
 mv -f CHANGELOG.md docs/
 
-echo "-o- Commit updates -o-"
+echo "\n-o- Commit updates -o-"
 git add optimade/__init__.py
 git add openapi/index_openapi.json openapi/openapi.json
 git add docs/CHANGELOG.md
-# git commit -m "Releasing ${GITHUB_REF#refs/tags/}"
-git commit -m "Releasing v0.10.0"
+git commit -m "Release ${GITHUB_REF#refs/tags/}"
