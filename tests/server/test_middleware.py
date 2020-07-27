3a2abecd7723880c0155e1ca5be85c0535c74812
Casper Welzel Andersen <casper.andersen@epfl.ch>
2020-07-27 21:22:14 +0200

    Tests of `AddWarnings`' helper methods

--- tests/server/test_middleware.py
+++ tests/server/test_middleware.py
@@ -151,3 +151,68 @@ def test_multiple_versions_in_path(both_clients):
     finally:
         if org_base_url:
             CONFIG.base_url = org_base_url
+
+
+def test_showwarning_overload(both_clients):
+    """Make sure warnings.showwarning can be overloaded correctly"""
+    import warnings
+
+    from optimade.server.middleware import AddWarnings
+    from optimade.server.warnings import OptimadeWarning
+
+    add_warning_middleware = AddWarnings(both_clients.app)
+    add_warning_middleware._warnings = []
+
+    warnings.showwarning = add_warning_middleware.showwarning
+
+    warning_message = "It's all gone awry!"
+
+    warnings.warn(OptimadeWarning(detail=warning_message))
+
+    assert add_warning_middleware._warnings == [
+        {"title": OptimadeWarning.__name__, "detail": warning_message}
+    ]
+
+
+def test_showwarning_debug(both_clients):
+    """Make sure warnings.showwarning adds 'meta' field in DEBUG MODE"""
+    import warnings
+
+    from optimade.server.config import CONFIG
+    from optimade.server.middleware import AddWarnings
+    from optimade.server.warnings import OptimadeWarning
+
+    add_warning_middleware = AddWarnings(both_clients.app)
+    add_warning_middleware._warnings = []
+
+    warnings.showwarning = add_warning_middleware.showwarning
+
+    warning_message = "It's all gone awry!"
+
+    org_debug = CONFIG.debug
+    try:
+        CONFIG.debug = True
+
+        warnings.warn(OptimadeWarning(detail=warning_message))
+
+        assert add_warning_middleware._warnings != [
+            {"title": OptimadeWarning.__name__, "detail": warning_message}
+        ]
+        warning = add_warning_middleware._warnings[0]
+        assert "meta" in warning
+        for meta_field in ("filename", "lineno", "line"):
+            assert meta_field in warning["meta"]
+    finally:
+        CONFIG.debug = org_debug
+
+
+def test_chunk_it_up():
+    """Make sure all content is return from `chunk_it_up` generator"""
+    from optimade.server.middleware import AddWarnings
+
+    content = "Oh what a sad and tragic waste of a young attractive life!"
+    chunk_size = 16
+    assert len(content) % chunk_size != 0, "We want a rest bit, this isn't helpful!"
+
+    generator = AddWarnings.chunk_it_up(content, chunk_size)
+    assert "".join(generator) == content
