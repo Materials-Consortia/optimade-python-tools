eddaf86fad7527fec3a464083766de67445ca61f
Matthew Evans <me388@cam.ac.uk>
2020-09-21 11:49:59 +0100

    Add queryable and support to field properties in entry info

--- optimade/server/schemas.py
+++ optimade/server/schemas.py
@@ -7,6 +7,20 @@ ENTRY_INFO_SCHEMAS = {
 
 
 def retrieve_queryable_properties(schema: dict, queryable_properties: list) -> dict:
+    """Recurisvely loops through the schema of a pydantic model and
+    resolves all references, returning a dictionary of all the
+    OPTIMADE-queryable properties of that model.
+
+    Parameters:
+        schema: The schema of the pydantic model.
+        queryable_properties: The list of properties to find in the schema.
+
+    Returns:
+        A flat dictionary with properties as keys, containing the field
+        description, unit, sortability, support level, queryability
+        and type, where provided.
+
+    """
     properties = {}
     for name, value in schema["properties"].items():
         if name in queryable_properties:
@@ -22,13 +36,15 @@ def retrieve_queryable_properties(schema: dict, queryable_properties: list) -> d
                 )
             else:
                 properties[name] = {"description": value.get("description", "")}
-                if "unit" in value:
-                    properties[name]["unit"] = value["unit"]
+                # Update schema with extension keys provided they are not None
+                for key in [_ for _ in ("unit", "queryable", "support") if _ in value]:
+                    properties[name][key] = value[key]
                 # All properties are sortable with the MongoDB backend.
                 # While the result for sorting lists may not be as expected, they are still sorted.
-                properties[name]["sortable"] = True
+                properties[name]["sortable"] = value.get("sortable", True)
                 # Try to get OpenAPI-specific "format" if possible, else get "type"; a mandatory OpenAPI key.
                 properties[name]["type"] = DataType.from_json_type(
                     value.get("format", value["type"])
                 )
+
     return properties
