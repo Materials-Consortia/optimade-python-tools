da942cd984a70675f0e0e679c1f7ae277a525d83
shyamd <shyamd@lbl.gov>
2020-09-23 17:56:53 -0700

    fix endpoint urls in static landing page

--- optimade/server/routers/landing.py
+++ optimade/server/routers/landing.py
@@ -1,12 +1,13 @@
 """ OPTIMADE landing page, rendered as a Jinja2 template. """
 
+import urllib
 from pathlib import Path
 from fastapi.templating import Jinja2Templates
 from starlette.routing import Router, Route
 from optimade import __api_version__
 
 from optimade.server.routers import ENTRY_COLLECTIONS
-from optimade.server.routers.utils import meta_values
+from optimade.server.routers.utils import meta_values, get_base_url
 from optimade.server.config import CONFIG
 
 template_dir = Path(__file__).parent.joinpath("static").resolve()
@@ -17,13 +18,9 @@ async def landing(request):
     """ Show a human-readable landing page when the base URL is accessed. """
 
     meta = meta_values(str(request.url), 1, 1, more_data_available=False)
-
+    parse_result = urllib.parse.urlparse(str(request.url))
     major_version = __api_version__.split(".")[0]
-    versioned_url = (
-        f"{request.url}"
-        if f"v{major_version}" in f"{request.url.path}"
-        else f"{request.url}v{major_version}/"
-    )
+    versioned_url = f"{get_base_url(parse_result)}/v{major_version}/"
 
     context = {
         "request": request,
