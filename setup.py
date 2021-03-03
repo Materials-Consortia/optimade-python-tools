bf07e4999dc1c97e117b66896107efe9ae154cc1
Casper Welzel Andersen <casper.andersen@epfl.ch>
2021-03-03 14:44:40 +0100

    Update setup.py and OpenAPI schemas
    The latter is needed due to an updated serialization of Enums used in
    pydantic models in pydantic>=1.7.

--- setup.py
+++ setup.py
@@ -93,7 +93,7 @@ setup(
     install_requires=[
         "lark-parser~=0.11.2",
         "fastapi~=0.63.0",
-        "pydantic~=1.6.1",
+        "pydantic~=1.8",
         "email_validator~=1.1",
         "requests~=2.25",
         "typing-extensions~=3.7;python_version<'3.8'",
