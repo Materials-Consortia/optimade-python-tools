afa4efe5d96f04fe553f207dc38a955912fcbd02
Matthew Evans <git@ml-evs.science>
2021-03-23 17:38:48 +0000

    Drop Python 3.6 support (closes #749)
    - Remove Python 3.6 deprecation warning
    
    Co-authored-by: Casper Welzel Andersen <CasperWA@users.noreply.github.com>

--- optimade/__init__.py
+++ optimade/__init__.py
@@ -1,21 +1,2 @@
 __version__ = "0.14.1"
 __api_version__ = "1.0.1"
-
-
-import sys
-
-if sys.version_info.minor == 6:
-    # Python 3.6
-    import warnings
-
-    warnings.filterwarnings(
-        action="once",
-        message=r"v0\.14 of the `optimade` package.*",
-        category=DeprecationWarning,
-        append=False,
-    )
-    warnings.warn(
-        "v0.14 of the `optimade` package will be the last to support Python 3.6. "
-        "Please upgrade to Python 3.7+ to use v0.15 and later versions of `optimade`.",
-        DeprecationWarning,
-    )
