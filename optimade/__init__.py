9048578e10b41a282dc4be46d4aaba0b1c9f61cc
Casper Welzel Andersen <casper.andersen@epfl.ch>
2021-03-24 17:42:28 +0100

    Improve deprecation warning

--- optimade/__init__.py
+++ optimade/__init__.py
@@ -4,17 +4,18 @@ __api_version__ = "1.0.1"
 
 import sys
 
-
-class OptimadeDeprecationWarning(Warning):
-    """Special deprecation warning"""
-
-
 if sys.version_info.minor == 6:
     # Python 3.6
     import warnings
 
+    warnings.filterwarnings(
+        action="once",
+        message=r"v0\.14 of the `optimade` package.*",
+        category=DeprecationWarning,
+        append=False,
+    )
     warnings.warn(
         "v0.14 of the `optimade` package will be the last to support Python 3.6. "
         "Please upgrade to Python 3.7+ to use v0.15 and later versions of `optimade`.",
-        OptimadeDeprecationWarning,
+        DeprecationWarning,
     )
