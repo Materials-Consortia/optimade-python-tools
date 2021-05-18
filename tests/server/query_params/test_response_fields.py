ffe4946f98504ba9b7ac3cfb7baf7e253cd357c7
Matthew Evans <git@ml-evs.science>
2021-05-18 22:26:50 +0100

    Additional tests for unknown response_fields

--- tests/server/query_params/test_response_fields.py
+++ tests/server/query_params/test_response_fields.py
@@ -25,3 +25,43 @@ def test_required_fields_structures(check_required_fields_response):
     non_used_top_level_fields = {"links"}
     expected_fields = {"elements", "nelements"}
     check_required_fields_response(endpoint, non_used_top_level_fields, expected_fields)
+
+
+def test_unknown_field_structures(
+    check_required_fields_response, check_error_response, check_response, structures
+):
+    """Check that unknown fields are returned as `null` in entries."""
+    endpoint = "structures"
+    non_used_top_level_fields = {"links"}
+
+    expected_fields = {"_optimade_field"}
+    check_required_fields_response(endpoint, non_used_top_level_fields, expected_fields)
+
+    expected_fields = {"optimade_field"}
+    check_error_response(
+        request=f"/{endpoint}?response_fields={','.join(expected_fields)}",
+        expected_status=400,
+        expected_title="Bad Request",
+        expected_detail="Unrecognised OPTIMADE field(s) in requested `response_fields`: {'optimade_field'}.",
+    )
+
+    expected_fields = {"_exmpl_optimade_field"}
+    expected_ids = [doc["task_id"] for doc in structures]
+    expected_warnings = [
+        {
+            "title": "UnknownProviderProperty",
+            "detail": "Unrecognised field(s) for this provider requested in `response_fields`: {'_exmpl_optimade_field'}.",
+        }
+    ]
+    check_response(
+        request=f"/{endpoint}?response_fields={','.join(expected_fields)}",
+        expected_ids=expected_ids,
+        expected_warnings=expected_warnings,
+    )
+
+    expected_fields = {"_exmpl123_optimade_field"}
+    expected_ids = [doc["task_id"] for doc in structures]
+    check_response(
+        request=f"/{endpoint}?response_fields={','.join(expected_fields)}",
+        expected_ids=expected_ids,
+    )
