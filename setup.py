e765b95f32c45bc83ef3be672933abe758bbf533
Casper Welzel Andersen <casper.w.andersen@sintef.no>
2021-12-08 10:37:28 +0100

    Support latest pymongo v3 version (as well as v4)

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open(module_dir.joinpath("optimade/__init__.py")) as version_file:
 # Dependencies
 # Server minded
 elastic_deps = ["elasticsearch-dsl~=6.4,<7.0"]
-mongo_deps = ["pymongo~=4.0", "mongomock~=3.23"]
+mongo_deps = ["pymongo>=3.12.1,<5", "mongomock~=3.23"]
 server_deps = [
     "uvicorn~=0.15",
     "Jinja2>=2.10,<4.0",
