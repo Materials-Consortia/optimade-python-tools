fa4d41abbd67711a04ccd945553a47eb680fa171
Johan Bergsma <29785380+JPBergsma@users.noreply.github.com>
2022-02-02 22:18:00 +0100

    Set insert_test_data to False.

--- optimade/server/config.py
+++ optimade/server/config.py
@@ -141,7 +141,7 @@ class ServerConfig(BaseSettings):
     )
 
     insert_test_data: bool = Field(
-        True,
+        False,
         description=(
             "Insert test data into each collection on server initialisation. If true, the "
             "configured backend will be populated with test data on server start. Should be "
