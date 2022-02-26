788b1e429ad330b5d3b7a1a5727072247f96fd31
Matthew Evans <git@ml-evs.science>
2022-02-26 16:22:47 +0000

    Do not use Jinja to generate the landing page

--- optimade/server/routers/landing.py
+++ optimade/server/routers/landing.py
@@ -1,9 +1,10 @@
-""" OPTIMADE landing page, rendered as a Jinja2 template. """
+""" OPTIMADE landing page router. """
 
 from pathlib import Path
+from functools import lru_cache
 
 from fastapi import Request
-from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 from starlette.routing import Router, Route
 from optimade import __api_version__
 
@@ -11,29 +12,77 @@ from optimade.server.routers import ENTRY_COLLECTIONS
 from optimade.server.routers.utils import meta_values, get_base_url
 from optimade.server.config import CONFIG
 
-template_dir = Path(__file__).parent.joinpath("static").resolve()
-TEMPLATES = Jinja2Templates(directory=[template_dir])
+
+@lru_cache()
+def render_landing_page(url: str) -> HTMLResponse:
+    """Render and cache the landing page.
+
+    This function uses the template file `./static/landing_page.html`, adapted
+    from the original Jinja template. Instead of Jinja, some basic string
+    replacement is used to fill out the fields from the server configuration.
+
+    !! warning
+    The removal of Jinja means that the fields are no longer validated as
+    web safe before inclusion in the template.
+
+    """
+    meta = meta_values(url, 1, 1, more_data_available=False)
+    major_version = __api_version__.split(".")[0]
+    versioned_url = f"{get_base_url(url)}/v{major_version}/"
+
+    template_dir = Path(__file__).parent.joinpath("static").resolve()
+
+    with open(template_dir / "landing_page.html", "r") as f:
+        html = "\n".join(f.readlines())
+
+    # Build a dictionary that maps the old Jinja keys to the new simplified replacements
+    replacements: dict[str, str] = {
+        "api_version": __api_version__,
+    }
+
+    if meta.provider:
+        replacements.update(
+            {
+                "provider.name": meta.provider.name,
+                "provider.prefix": meta.provider.prefix,
+                "provider.description": meta.provider.description,
+                "provider.homepage": str(meta.provider.homepage) or "",
+            }
+        )
+
+    if meta.implementation:
+        replacements.update(
+            {
+                "implementation.name": meta.implementation.name or "",
+                "implementation.version": meta.implementation.version or "",
+                "implementation.source_url": str(meta.implementation.source_url or ""),
+            }
+        )
+
+    for replacement in replacements:
+        html = html.replace(f"{{{{ {replacement} }}}}", replacements[replacement])
+
+    # Build the list of endpoints. The template already opens and closes the `<ul>` tag.
+    endpoints_list = [
+        f'<li><a href="{versioned_url}{endp}">{versioned_url}{endp}'
+        for endp in list(ENTRY_COLLECTIONS.keys()) + ["info"]
+    ]
+    html = html.replace("{% ENDPOINTS %}", "\n".join(endpoints_list))
+
+    # If the index base URL has been configured, also list it
+    index_base_url_html = ""
+    if CONFIG.index_base_url:
+        index_base_url_html = f"""<h3>Index base URL:</h3>
+<p><a href={CONFIG.index_base_url}>{CONFIG.index_base_url}</a></p>
+"""
+    html = html.replace("{% INDEX_BASE_URL %}", index_base_url_html)
+
+    return HTMLResponse(html)
 
 
 async def landing(request: Request):
     """Show a human-readable landing page when the base URL is accessed."""
-
-    meta = meta_values(request.url, 1, 1, more_data_available=False)
-    major_version = __api_version__.split(".")[0]
-    versioned_url = f"{get_base_url(request.url)}/v{major_version}/"
-
-    context = {
-        "request": request,
-        "request_url": request.url,
-        "api_version": __api_version__,
-        "implementation": meta.implementation,
-        "versioned_url": versioned_url,
-        "provider": meta.provider,
-        "index_base_url": CONFIG.index_base_url,
-        "endpoints": list(ENTRY_COLLECTIONS.keys()) + ["info"],
-    }
-
-    return TEMPLATES.TemplateResponse("landing_page.html", context)
+    return render_landing_page(str(request.url))
 
 
 router = Router(routes=[Route("/", endpoint=landing)])
