4a7eff74b94547a852d657d53d23d830082f7671
Johan Bergsma <29785380+JPBergsma@users.noreply.github.com>
2022-05-03 09:39:15 +0200

    Apply suggestions from code review
    Co-authored-by: Matthew Evans <7916000+ml-evs@users.noreply.github.com>

--- optimade/server/query_params.py
+++ optimade/server/query_params.py
@@ -215,7 +215,7 @@ class EntryListingQueryParams(BaseQueryParams):
         page_number: int = Query(
             None,
             description="RECOMMENDED for use with _page-based_ pagination: using `page_number` and `page_limit` is RECOMMENDED.\nIt is RECOMMENDED that the first page has number 1, i.e., that `page_number` is 1-based.\nExample: Fetch page 2 of up to 50 structures per page: `/structures?page_number=2&page_limit=50`.",
-            ge=0,
+            ge=1,
         ),
         page_cursor: int = Query(
             0,
