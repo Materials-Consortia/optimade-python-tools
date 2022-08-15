30af05ac879c63e57ea30a3bf9e9cf4df3125e21
Johan Bergsma <29785380+JPBergsma@users.noreply.github.com>
2022-08-15 15:53:32 +0200

    Added filenames to the header.

--- optimade/server/routers/utils.py
+++ optimade/server/routers/utils.py
@@ -285,7 +285,9 @@ def get_entries(
             return Response(
                 content=generate_hdf5_file_content(response_object),
                 media_type="application/x-hdf5",
-                headers={"Content-Disposition": "attachment"},
+                headers={
+                    "Content-disposition": f"attachment; filename={collection.collection.name}.hdf5"
+                },
             )
     else:
         raise BadRequest(
@@ -346,7 +348,9 @@ def get_single_entry(
             return Response(
                 content=generate_hdf5_file_content(response_object),
                 media_type="application/x-hdf5",
-                headers={"Content-Disposition": "attachment"},
+                headers={
+                    "Content-disposition": f"attachment; filename={entry_id}.hdf5"
+                },
             )
     else:
         raise BadRequest(
