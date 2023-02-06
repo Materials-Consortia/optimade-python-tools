4a166fbcff7c009361c8900ba56535f7a7be2fd1
Johan Bergsma <29785380+JPBergsma@users.noreply.github.com>
2023-02-06 18:12:02 +0100

    Merge branch 'master' into JPBergsma_add_trajectory
    # Conflicts:
    #	openapi/openapi.json
    #	optimade/models/__init__.py
    #	optimade/models/jsonapi.py
    #	optimade/models/structures.py
    #	optimade/server/mappers/__init__.py
    #	optimade/server/middleware.py
    #	optimade/server/routers/info.py
    #	optimade/server/routers/utils.py
    #	optimade/server/schemas.py
    #	optimade/server/warnings.py
    #	optimade/validator/config.py
    #	requirements.txt
    #	setup.py
    #	tests/server/conftest.py
    #	tests/server/test_mappers.py

--- optimade/server/config.py
+++ optimade/server/config.py
@@ -314,8 +314,8 @@ class ServerConfig(BaseSettings):
         ["json"],
         description="""A list of the response formats that are supported by this server. Must include the "json" format.""",
     )
-    max_response_size: Dict[SupportedResponseFormats, int] = Field(
-        {"json": 10},
+    max_response_size: Optional[Dict[SupportedResponseFormats, int]] = Field(
+        {"json": 10, "hdf5": 40},
         description="""This dictionary contains the approximate maximum size for a trajectory response in megabytes for the different response_formats. The keys indicate the response_format and the values the maximum size.""",
     )
 
