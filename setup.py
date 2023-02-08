7c8f9a63ff169fd1e1c90676e304ca9c99e5b939
Matthew Evans <7916000+ml-evs@users.noreply.github.com>
2023-02-08 19:20:19 +0000

    Tweak pymatgen pin in `setup.py` to allow for calver (#1505)
    * Relax numpy pin
    
    * Relax pymatgen pin as they use calver
    
    * Linting

--- setup.py
+++ setup.py
@@ -32,9 +32,9 @@ http_client_deps = [
     "click~=8.1",
 ]
 ase_deps = ["ase~=3.22"]
-cif_deps = ["numpy>=1.23"]
+cif_deps = ["numpy>=1.20"]
 pdb_deps = cif_deps
-pymatgen_deps = ["pymatgen~=2022.7"]
+pymatgen_deps = ["pymatgen>=2022"]
 jarvis_deps = ["jarvis-tools==2023.1.8"]
 client_deps = cif_deps
 
