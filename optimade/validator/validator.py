b561cff3cf07454f009bc47c578896cef51ec3f9
munahaf <munawar.hafiz@gmail.com>
2023-10-25 09:20:08 +0000

    Comment: Fixed an inappropriate test expression to remove a logical short circuit.

--- optimade/validator/validator.py
+++ optimade/validator/validator.py
@@ -561,7 +561,7 @@ class ImplementationValidator:
         test_query = f"{endp}?response_fields={','.join(subset_fields)}&page_limit=1"
         response, _ = self._get_endpoint(test_query, multistage=True)
 
-        if response and len(response.json()["data"]) >= 0:
+        if response and len(response.json()["data"]) > 0:
             doc = response.json()["data"][0]
             expected_fields = set(subset_fields)
             expected_fields -= CONF.top_level_non_attribute_fields
