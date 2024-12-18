f93053663cb65ba95297fcdb5eadebbc1fd46c88
Johan Bergsma <29785380+JPBergsma@users.noreply.github.com>
2024-12-18 13:39:12 +0100

    Catch error for invalid URI.
    This is a second attempt to resolve issue#2197 and should enable connecting to a MongoDB Atlas Cluster via its connection string.

--- optimade/server/config.py
+++ optimade/server/config.py
@@ -497,12 +497,17 @@ class ServerConfig(BaseSettings):
                     "mongodb://"
                 ) or self.mongo_uri.startswith("mongodb+srv://"):
                     self.mongo_uri = f"mongodb://{self.mongo_uri}"
+                try:
+                    uri: dict[str, Any] = pymongo.uri_parser.parse_uri(
+                        self.mongo_uri, warn=True
+                    )
+                    if uri.get("database"):
+                        self.mongo_database = uri["database"]
+                except InvalidURI as error_msg:
+                    warnings.warn(
+                        f"The uri {self.mongo_uri} may be invalid.\n{error_msg}"
+                    )
 
-                uri: dict[str, Any] = pymongo.uri_parser.parse_uri(
-                    self.mongo_uri, warn=True
-                )
-                if uri.get("database"):
-                    self.mongo_database = uri["database"]
 
         return self
 
