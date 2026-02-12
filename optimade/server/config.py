b43c00d031d02eb7ec359904d20b1e72a6882906
Matthew Evans <git@ml-evs.science>
2026-02-12 18:17:11 +0000

    Try adding sortable to allowed fields in config

--- optimade/server/config.py
+++ optimade/server/config.py
@@ -368,7 +368,7 @@ Otherwise, the license will be given as the provided URL and no SPDX identifier 
     provider_fields: Annotated[
         dict[
             Literal["links", "references", "structures"],
-            list[str | dict[Literal["name", "type", "unit", "description"], str]],
+            list[str | dict[Literal["name", "type", "unit", "description", "sortable"], str]],
         ],
         Field(
             default_factory=dict,
